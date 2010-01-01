e639cfdef4ebc2ade97b4f517b124a61fe7e9829
Jiri Bluebear Dluhos <bluebear@grindelwald.site>
2010-01-02 00:30:16 +0100

    Improved speed of drawing of 24-bit HLine().

--- generic/hline_generic.c
+++ generic/hline_generic.c
@@ -105,6 +105,127 @@ void FN_NAME(GP_TARGET_TYPE *target, long color, int x0, int x1, int y)
 		}
 	}
 
+#elif BYTES_PER_PIXEL == 3
+
+	uint8_t *bytep = GP_PIXEL_ADDR(target, x0, y);
+
+	/* How much bytes we are offset against the 32-bit boundary. */
+	int shift = ((intptr_t) bytep) % 4;
+
+	/* How much pixels must be drawn in total. */
+	int pixelcount = 1 + x1 - x0;
+	int i;
+
+	/*
+	 * Handle each color component separately.
+	 * (Probably they are R, G, B but who knows.)
+	 */
+	uint8_t a = color & 0xff;
+	uint8_t b = (color >> 8) & 0xff;
+	uint8_t c = (color >> 16) & 0xff;
+
+	uint32_t *p;
+	uint32_t block[3];
+
+	/*
+	 * The line consists of three repeating 32-bit segments
+	 * (except for the starting and ending segment:
+	 * ABCA, BCAB, CABC.
+	 */
+#if __BIG_ENDIAN__
+	uint32_t abca = a << 24 | b << 16 | c << 8 | a;
+	uint32_t bcab = b << 24 | c << 16 | a << 8 | b;
+	uint32_t cabc = c << 24 | a << 16 | b << 8 | c;
+#else
+	uint32_t abca = a << 24 | c << 16 | b << 8 | a;
+	uint32_t bcab = b << 24 | a << 16 | c << 8 | b;
+	uint32_t cabc = c << 24 | b << 16 | a << 8 | c;
+#endif
+
+	/*
+	 * Handle the first few bytes (1 pixel or less) and prepare
+	 * the repeating sequence.
+	 */
+	switch (shift) {
+	case 0:
+		block[0] = abca;
+		block[1] = bcab;
+		block[2] = cabc;
+		p = (uint32_t *) bytep;
+		i = pixelcount;
+		break;
+	case 3:
+		bytep[0] = a;
+		block[0] = bcab;
+		block[1] = cabc;
+		block[2] = abca;
+		p = (uint32_t *)(bytep + 1);
+		i = pixelcount - 1;
+		break;
+	case 2:
+		bytep[0] = a;
+		bytep[1] = b;
+		block[0] = cabc;
+		block[1] = abca;
+		block[2] = bcab;
+		p = (uint32_t *)(bytep + 2);
+		i = pixelcount - 2;
+		break;
+	case 1:
+		bytep[0] = a;
+		bytep[1] = b;
+		bytep[2] = c;
+		block[0] = abca;
+		block[1] = bcab;
+		block[2] = cabc;
+		p = (uint32_t *)(bytep + 3);
+		i = pixelcount - 3;
+		break;
+	}
+
+	/* 
+	 * Write as much of the line as possible as
+	 * triplets of 32-bit numbers; hopefully the compiler can
+	 * put some wide write instructions in.
+	 */
+	while (i >= 4) {
+		p[0] = block[0];
+		p[1] = block[1];
+		p[2] = block[2];
+		p += 3;
+		i -= 4;
+	}
+
+	/* Write the rest of the last pixel of the main part */
+	bytep = (uint8_t *) p;
+	switch (shift) {
+	case 1:
+		bytep[0] = a;
+		bytep[1] = b;
+		bytep[2] = c;
+		bytep += 3;
+		break;
+	case 0:
+		break;
+	case 2:
+		bytep[0] = c;
+		bytep++;
+		break;
+	case 3:
+		bytep[0] = b;
+		bytep[1] = c;
+		bytep += 2;
+		break;
+	}
+
+	while (i > 0) {
+		bytep[0] = a;
+		bytep[1] = b;
+		bytep[2] = c;
+		bytep += 3;
+		i--;
+	}
+
 #elif BYTES_PER_PIXEL == 2
 
 	uint16_t *p = (uint16_t *) GP_PIXEL_ADDR(target, x0, y);
