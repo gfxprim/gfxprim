11a833ae080a2aae4c37ffd2479faf25eb3ff1a3
metan <metan@ucw.cz>
2011-02-21 17:37:57 +0100

    Started to write bitmap loaders.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 HEADER_LOC=/usr/include/
 LIB_LOC=/usr/lib/
 
-.PHONY: all core sdl drivers install clean tar
+.PHONY: all core sdl drivers loaders install clean tar
 
-all: core sdl drivers
+all: core sdl drivers loaders
 
 drivers:
 	cd drivers && $(MAKE) all
@@ -16,11 +16,18 @@ core:
 sdl: core
 	cd targets/sdl && $(MAKE) all
 
+loaders:
+	cd loaders && $(MAKE) all
+
 install: all
 	# core library
 	install -m 775 -d $(HEADER_LOC)GP/
 	install -m 664 core/*.h $(HEADER_LOC)GP/
 	install -m 664 core/*.so core/*.so.0 core/*.a $(LIB_LOC)
+	# bitmap loaders
+	install -m 775 -d $(HEADER_LOC)GP/loaders/
+	install -m 664 loaders/*.h $(HEADER_LOC)GP/loaders/
+	install -m 664 loaders/*.so loaders/*.so.0 loaders/*.a $(LIB_LOC)
 	# sdl target	
 	install -m 775 -d $(HEADER_LOC)GP/SDL/
 	install -m 664 targets/sdl/*.h $(HEADER_LOC)GP/SDL/
@@ -32,6 +39,7 @@ clean:
 	cd core && $(MAKE) clean
 	cd targets/sdl && $(MAKE) clean
 	cd drivers && $(MAKE) clean
+	cd loaders && $(MAKE) clean
 
 tar: clean
 	cd .. && tar cjf gfxprim-`date +%Y-%b-%d-%HH%MM`.tar.bz2 gfxprim
---
+++ loaders/tests/PBM_test.c
@@ -0,0 +1,56 @@
+/*****************************************************************************
+ * This file is part of gfxprim library.                                     *
+ *                                                                           *
+ * Gfxprim is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                *
+ * License as published by the Free Software Foundation; either              *
+ * version 2.1 of the License, or (at your option) any later version.        *
+ *                                                                           *
+ * Gfxprim is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Lesser General Public License for more details.                           *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public          *
+ * License along with gfxprim; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                        *
+ * Boston, MA  02110-1301  USA                                               *
+ *                                                                           *
+ * Copyright (C) 2009-2010 Jiri "BlueBear" Dluhos                            *
+ *                         <jiri.bluebear.dluhos@gmail.com>                  *
+ *                                                                           *
+ * Copyright (C) 2009-2010 Cyril Hrubis <metan@ucw.cz>                       *
+ *                                                                           *
+ *****************************************************************************/
+
+ /*
+
+  */
+
+#include <GP.h>
+#include <GP_PBM.h>
+
+int main(void)
+{
+	GP_Context *context = GP_ContextAlloc(11, 13, GP_PIXEL_G1);
+
+	if (context == NULL) {
+		fprintf(stderr, "Couldn't allocate context\n");
+		return 1;
+	}
+
+	GP_Pixel pix = {1, GP_PIXEL_G1};
+
+	if (GP_Line(context, 0, 0, 11, 13, pix)) {
+		fprintf(stderr, "GP_Line not implemented for G1\n");
+	}
+
+	if (GP_SavePBM("test.pbm", context)) {
+		fprintf(stderr, "Can't save context\n");
+		return 1;
+	}
+
+	GP_ContextFree(context);
+
+	return 0;
+}
