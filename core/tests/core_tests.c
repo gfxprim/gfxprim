2445c17c5ef46920b04e732e2792844ca26567fc
Tomas Gavenciak <gavento@ucw.cz>
2011-03-16 00:47:23 +0100

    Introduce Check tests

---
+++ core/tests/core_tests.c
@@ -0,0 +1,74 @@
+/*****************************************************************************
+ * This file is part of gfxprim library.                                     *
+ *                                                                           *
+ * Gfxprim is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                *
+ * License as published by the Free Software Foundation; either              *
+ * version 2.1 of the License, or (at your option) any later version.        *
+ *                                                                           *
+ * Gfxprim is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Lesser General Public License for more details.                           *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public          *
+ * License along with gfxprim; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                        *
+ * Boston, MA  02110-1301  USA                                               *
+ *                                                                           *
+ * Copyright (C) 2011 Tomas Gavenciak <gavento@ucw.cz>                       *
+ *                                                                           *
+ *****************************************************************************/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <check.h>
+
+typedef Suite* (SuiteFactory)(void);
+
+/* 
+ * Declare all the testcase-generating functions here:
+ */
+
+Suite *TS_Common(void);
+
+SuiteFactory* suitas[] = {
+	TS_Common,
+	NULL	/* Sentinel */
+};
+
+
+const char usage[] = "Usage:\n%s [-v] [-q]\n";
+
+int main(int argc, char **argv)
+{
+	int verb = CK_NORMAL;
+
+	int opt;	
+	while((opt = getopt(argc, argv, "vq")) >= 0) 
+		switch(opt) {
+			case 'v': 
+				verb = CK_VERBOSE;
+				break;
+			case 'q':
+				verb = CK_SILENT;
+				break;
+			default:
+				fprintf(stderr, usage, argv[0]);
+				return(EXIT_FAILURE);
+		}
+
+	SRunner *sr = srunner_create(NULL);
+	for (SuiteFactory **s = suitas; *s; s++)
+	{
+		srunner_add_suite(sr, (*s)());
+	}
+	
+	srunner_run_all(sr, verb);
+	int number_failed = srunner_ntests_failed(sr);
+	srunner_free(sr);
+	
+	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
