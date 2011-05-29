4920417b0a6d627b8d44d50a79ffe53419cd50d5
Tomas Gavenciak <gavento@ucw.cz>
2011-05-30 01:03:16 +0200

    Make FnPerBPP depend on bit-endian

--- include/core/GP_FnPerBpp.h
+++ include/core/GP_FnPerBpp.h
@@ -29,79 +29,79 @@
  * Extra arguments are arguments to be passed to the function.
  * Returns GP_ENOIMPL if the bit depth is unknown.
  */
-#define GP_FN_PER_BPP(FN_NAME, bpp, ...) \
+#define GP_FN_PER_BPP(FN_NAME, bpp, bit_endian, ...) \
 \
 	switch (bpp) { \
 	case 1: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			FN_NAME##1bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			FN_NAME##_1BPP_LE(__VA_ARGS__);\
 		else \
-			FN_NAME##1bpp_BE(__VA_ARGS__);\
+			FN_NAME##_1BPP_BE(__VA_ARGS__);\
 		} \
 	break; \
 	case 2: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			FN_NAME##2bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			FN_NAME##_2BPP_LE(__VA_ARGS__);\
 		else \
-			FN_NAME##2bpp_BE(__VA_ARGS__);\
+			FN_NAME##_2BPP_BE(__VA_ARGS__);\
 		} \
 	break; \
 	case 4: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			FN_NAME##4bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			FN_NAME##_4BPP_LE(__VA_ARGS__);\
 		else \
-			FN_NAME##4bpp_BE(__VA_ARGS__);\
+			FN_NAME##_4BPP_BE(__VA_ARGS__);\
 		} \
 	break; \
 	case 8: \
-		FN_NAME##8bpp(__VA_ARGS__); \
+		FN_NAME##_8BPP(__VA_ARGS__); \
 	break; \
 	case 16: \
-		FN_NAME##16bpp(__VA_ARGS__); \
+		FN_NAME##_16BPP(__VA_ARGS__); \
 	break; \
 	case 24: \
-		FN_NAME##24bpp(__VA_ARGS__); \
+		FN_NAME##_24BPP(__VA_ARGS__); \
 	break; \
 	case 32: \
-		FN_NAME##32bpp(__VA_ARGS__); \
+		FN_NAME##_32BPP(__VA_ARGS__); \
 	break; \
 	default: \
 	break; \
 	} \
 
-#define GP_FN_RET_PER_BPP(FN_NAME, bpp, ...) \
+#define GP_FN_RET_PER_BPP(FN_NAME, bpp, bit_endian, ...) \
 \
 	switch (bpp) { \
 	case 1: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			return FN_NAME##1bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			return FN_NAME##_1BPP_LE(__VA_ARGS__);\
 		else \
-			return FN_NAME##1bpp_BE(__VA_ARGS__);\
+			return FN_NAME##_1BPP_BE(__VA_ARGS__);\
 		} \
 	case 2: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			return FN_NAME##2bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			return FN_NAME##_2BPP_LE(__VA_ARGS__);\
 		else \
-			return FN_NAME##2bpp_BE(__VA_ARGS__);\
+			return FN_NAME##_2BPP_BE(__VA_ARGS__);\
 		} \
 	case 4: \
 		{\
-		if (context->bit_endian==GP_BIT_ENDIAN_LE)\
-			return FN_NAME##4bpp_LE(__VA_ARGS__);\
+		if (bit_endian==GP_BIT_ENDIAN_LE)\
+			return FN_NAME##_4BPP_LE(__VA_ARGS__);\
 		else \
-			return FN_NAME##4bpp_BE(__VA_ARGS__);\
+			return FN_NAME##_4BPP_BE(__VA_ARGS__);\
 		} \
 	case 8: \
-		return FN_NAME##8bpp(__VA_ARGS__); \
+		return FN_NAME##_8BPP(__VA_ARGS__); \
 	case 16: \
-		return FN_NAME##16bpp(__VA_ARGS__); \
+		return FN_NAME##_16BPP(__VA_ARGS__); \
 	case 24: \
-		return FN_NAME##24bpp(__VA_ARGS__); \
+		return FN_NAME##_24BPP(__VA_ARGS__); \
 	case 32: \
-		return FN_NAME##32bpp(__VA_ARGS__); \
+		return FN_NAME##_32BPP(__VA_ARGS__); \
 	}
