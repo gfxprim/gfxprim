ebd807bf4a3a9c1f916dff933c16536c4ef2261c
Tomas Gavenciak <gavento@ucw.cz>
2011-06-12 22:30:41 +0200

    Added helpers and documentation to FnPerBpp

--- include/core/GP_FnPerBpp.h
+++ include/core/GP_FnPerBpp.h
@@ -24,17 +24,44 @@
  *****************************************************************************/
 
 /*
- * Macro that generates a switch-case block that calls various variants
- * of the specified function depending on the bit depth of the context.
+ * Macros that generate a switch-case block that calls various variants
+ * of the specified function depending on the bit depth (bpp).
+ *
+ * For function name y and x bpp, the function called will be
+ * y_xBPP, resp y_xBPP_LE (or _BE; the bit-endian) for x<8.
+ *
+ * The functions branch either on bpp and bit_endian, type of given
+ * GP_Context or given GP_PixelType.
+ *
  * Extra arguments are arguments to be passed to the function.
- * Returns GP_ENOIMPL if the bit depth is unknown.
+ * Note that if the function takes the context/type/bpp as an argument, 
+ * you still need to provide it in __VA_ARGS__
+ *
+ * The GP_FN_PER_* variants ignore the return value of the called function.
+ * The GP_FN_RET_PER_* variants "return"s the value returned by the function.
+ */
+
+/*
+ * Branch on GP_Context argument. 
+ */
+#define GP_FN_PER_CONTEXT(FN_NAME, context, ...) \
+	GP_FN_PER_BPP(FN_NAME, (context)->bpp, (context)->bit_endian, __VA_ARGS__)
+
+/*
+ * Branch on GP_PixelType argument. 
+ */
+#define GP_FN_PER_PIXELTYPE(FN_NAME, type, ...) \
+	GP_FN_PER_BPP(FN_NAME, GP_PixelTypes[type].size, GP_PixelTypes[type].bit_endian, __VA_ARGS__)
+
+/*
+ * Branch on bpp and bit_endian. 
  */
 #define GP_FN_PER_BPP(FN_NAME, bpp, bit_endian, ...) \
 \
 	switch (bpp) { \
 	case 1: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			FN_NAME##_1BPP_LE(__VA_ARGS__);\
 		else \
 			FN_NAME##_1BPP_BE(__VA_ARGS__);\
@@ -42,7 +69,7 @@
 	break; \
 	case 2: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			FN_NAME##_2BPP_LE(__VA_ARGS__);\
 		else \
 			FN_NAME##_2BPP_BE(__VA_ARGS__);\
@@ -50,7 +77,7 @@
 	break; \
 	case 4: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			FN_NAME##_4BPP_LE(__VA_ARGS__);\
 		else \
 			FN_NAME##_4BPP_BE(__VA_ARGS__);\
@@ -72,26 +99,41 @@
 	break; \
 	} \
 
+/*
+ * Branch on GP_Context argument. 
+ */
+#define GP_FN_RET_PER_CONTEXT(FN_NAME, context, ...) \
+	GP_FN_RET_PER_BPP(FN_NAME, (context)->bpp, (context)->bit_endian, __VA_ARGS__)
+
+/*
+ * Branch on GP_PixelType argument. 
+ */
+#define GP_FN_RET_PER_PIXELTYPE(FN_NAME, type, ...) \
+	GP_FN_RET_PER_BPP(FN_NAME, GP_PixelTypes[type].size, GP_PixelTypes[type].bit_endian, __VA_ARGS__)
+
+/*
+ * Branch on bpp and bit_endian. 
+ */
 #define GP_FN_RET_PER_BPP(FN_NAME, bpp, bit_endian, ...) \
 \
 	switch (bpp) { \
 	case 1: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			return FN_NAME##_1BPP_LE(__VA_ARGS__);\
 		else \
 			return FN_NAME##_1BPP_BE(__VA_ARGS__);\
 		} \
 	case 2: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			return FN_NAME##_2BPP_LE(__VA_ARGS__);\
 		else \
 			return FN_NAME##_2BPP_BE(__VA_ARGS__);\
 		} \
 	case 4: \
 		{\
-		if (bit_endian==GP_BIT_ENDIAN_LE)\
+		if (bit_endian == GP_BIT_ENDIAN_LE)\
 			return FN_NAME##_4BPP_LE(__VA_ARGS__);\
 		else \
 			return FN_NAME##_4BPP_BE(__VA_ARGS__);\
