3cca227bd9d97802da766c2feca0bb537c038b16
Cyril Hrubis <metan@ucw.cz>
2011-11-24 22:35:34 +0100

    Add -fPIC to fix the compilation on x86_64.

--- include/loaders/swigify.sh
+++ include/loaders/swigify.sh
@@ -2,7 +2,7 @@ set -e
 
 swig -python -Wall -I/usr/include/ gfxprim_loaders.swig
 gcc -shared gfxprim_loaders_wrap.c -L ../../build/ -I /usr/include/python2.6/ -I.. \
-  -lGP -lpng -ljpeg -lm -ldl -o ../../build/_gfxprim_loaders.so
+  -fPIC -lGP -lpng -ljpeg -lm -ldl -o ../../build/_gfxprim_loaders.so
 mv gfxprim_loaders.py ../../build/
 rm gfxprim_loaders_wrap.c
 
