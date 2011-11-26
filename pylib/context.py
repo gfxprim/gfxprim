133b310ce8ca4adc61f14648dee634a00d5cf077
Tomas Gavenciak <gavento@ucw.cz>
2011-11-26 20:04:12 +0100

    Complete rewrite of the wrapper class
    Now extending the SWIG wrapper instead of delegation
    Decorating tools
    TODO: set docstring correctly

--- pylib/context.py
+++ pylib/context.py
@@ -1,58 +1,84 @@
 import gfxprim_core_c as core
 import gfxprim_loaders_c as loaders
 
-class Context(object):
-  def __init__(self, context, owns_GP_Context, parent=None):
-    """Create new GP_Context wrapper.
+def extend(cls, name=None):
+  def decf(method):
+    funname = name
+    if not funname:
+      funname = method.__name__
+    type.__setattr__(cls, funname, method)
+    return method
+  return decf
 
-    The wrapper is just a (read-only) proxy with 
-    some conveniently mapped context methods.
-    Direct access is possible through self.context.
-    """
-    # GP_Context struct (pre-wrapped by SWIG)
-    self.context = context
-    # call GP_ContextFree on del?
-    self.owns_GP_Context = owns_GP_Context
-    # Parent Context of a subcontext (if any)
-    # It is important to hold a ref to parent because of GC
-    self.parent = parent
+def add_swig_getmethod(cls, name=None):
+  def decf(method):
+    propname = name
+    if not propname:
+      propname = method.__name__
+    cls.__swig_getmethods__[propname] = method
+  return decf
 
-  def __del__(self):
-    print "Deleting %s (owns_GP_Context: %s)" % (self, self.owns_GP_Context)
-    if self.owns_GP_Context:
-      core.GP_ContextFree(self.context)
+def add_swig_setmethod(cls, name=None):
+  def decf(method):
+    propname = name
+    if not propname:
+      propname = method.__name__
+    cls.__swig_setmethods__[propname] = method
+  return decf
 
-  @property
-  def w(self):
-    "Context width (transformed)"
-    return core.GP_ContextW(self.context)
 
-  @property
-  def h(self):
-    "Context height (transformed)"
-    return core.GP_ContextH(self.context)
+def extend_context_class(_context_class = core.Context):
+  """
+  Extends _context_class class with convenience methods.
+  Called on module inicialization.
+  """
 
-  @property
-  def bpp(self):
-    "Context bpp"
-    return self.context.bpp
+  type.__setattr__(_context_class, "__doc_2__",
+    """Proxy of C GP_Context struct
 
-  def __str__(self):
+    You can pass this class to wrapped GP_DrawSomething(...) as GP_Context.
+    All attributes of GP_Context are accessible directly as "_attribute"
+    (self._w etc.), but it is reccomended to use redefined properties:
+
+     self.w: Context width (transformed)
+     self.h: Context width (transformed)
+
+    Some context methods are provided as class members for convenience.
+
+    GP_Context memory allocation is handled completely by gfxprim.
+    You shoud NEVER even think about self.this - this pointer will point to
+    free memory after self.__del__ is called (in case of owned GP_Context).
+    """)
+
+  extend(_context_class, name='parent')(None)
+
+  @extend(_context_class, name='__str__')
+  @extend(_context_class, name='__repr__')
+  def context_str(self):
     return "<Context %dx%d, %dbpp, GP_Context %sowned, %s parent>" % (
-	self.w, self.h, self.bpp,
-	"" if self.owns_GP_Context else "not ",
-	"with" if self.parent else "no")
-  __repr__ = __str__
+      self.w, self.h, self._bpp,
+      "" if self.thisown else "not ",
+      "with" if self.parent else "no")
 
+  @add_swig_getmethod(_context_class)
+  def w(self):
+    return core.GP_ContextW(self)
+
+  @add_swig_getmethod(_context_class)
+  def h(self):
+    return core.GP_ContextH(self)
+
+  @extend(_context_class)
   def subcontext(self, x, y, w, h):
     "Create a subcontext (rectangular view)."
-    c = core.GP_ContextSubContext(self.context, None, x, y, w, h)
-    if not c:
-      raise Exception("Error creating subcontext")
-    return type(self)(c, owns_GP_Context=True, parent=self)
+    c = core.GP_ContextSubContext(self, None, x, y, w, h)
+    c.thisown = True # GP_Context IS owned (but not the pixel data)
+    c.parent = self
+    return c
 
+  @extend(_context_class)
   def save(self, filename, format=None):
-    """Save the image in given format (or guess by the extension)
+    """Save the image in given format (or guess it from the extension)
 
     Currently, JPG, PNG and P[BGP]M are supported, but not for all 
     context pixel types.
@@ -61,34 +87,40 @@ class Context(object):
       format = filename.rsplit('.', 1)[-1]
     format = format.lower()
     if format == 'jpg':
-      res = loaders.GP_SaveJPG(filename, self.context, None)
+      res = loaders.GP_SaveJPG(filename, self, None)
     elif format == 'png':
-      res = loaders.GP_SavePNG(filename, self.context, None)
+      res = loaders.GP_SavePNG(filename, self, None)
     elif format == 'pbm':
-      res = loaders.GP_SavePBM(filename, self.context, None)
+      res = loaders.GP_SavePBM(filename, self, None)
     elif format == 'pgm':
-      res = loaders.GP_SavePGM(filename, self.context, None)
+      res = loaders.GP_SavePGM(filename, self, None)
     elif format == 'ppm':
-      res = loaders.GP_SavePPM(filename, self.context, None)
+      res = loaders.GP_SavePPM(filename, self, None)
     else:
       raise Exception("Format %r not supported.", format)
     if res != 0:
       raise Exception("Error saving %r (code %d)", filename, res)
 
-  @classmethod
-  def create(cls, w, h, pixeltype):
+  @extend(_context_class, name='create')
+  @staticmethod
+  def create(w, h, pixeltype):
     "Allocate a new w*h bitmap of given type."
 
     pixeltype_no = pixeltype if isinstance(pixeltype, int) else 0 # !!!
     # TODO: actually accept a PixelType
     c = core.GP_ContextAlloc(w, h, pixeltype_no)
-    return cls(c, owns_GP_Context=True)
+    c.thisown = True
+    return c
 
-  @classmethod
-  def load(cls, filename):
+  @extend(_context_class, name='load')
+  @staticmethod
+  def load(filename):
     "Load image from given file, guess type."
     c = loaders.GP_LoadImage_SWIG(filename)
-    if not c:
-      raise Exception("Error loading %r", filename)
-    return cls(c, owns_GP_Context=True)
+    c.thisown = True
+    return c
+
+
+
+extend_context_class()
 
