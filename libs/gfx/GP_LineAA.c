923ee620ce7cc8acf847c29b9da82ba5899ae520
Cyril Hrubis <metan@ucw.cz>
2012-02-13 14:44:05 +0100

    gfx: Added header for Anti Aliased line, finally.

--- libs/gfx/GP_LineAA.c
+++ libs/gfx/GP_LineAA.c
@@ -19,41 +19,33 @@
  * Copyright (C) 2009-2010 Jiri "BlueBear" Dluhos                            *
  *                         <jiri.bluebear.dluhos@gmail.com>                  *
  *                                                                           *
- * Copyright (C) 2009-2011 Cyril Hrubis <metan@ucw.cz>                       *
+ * Copyright (C) 2009-2012 Cyril Hrubis <metan@ucw.cz>                       *
  *                                                                           *
  *****************************************************************************/
 
+#include "core/GP_Transform.h"
+#include "core/GP_FnPerBpp.h"
+
+#include "gfx/GP_LineAA.h"
+
 /*
+void GP_Line_Raw(GP_Context *context, GP_Coord x0, GP_Coord y0,
+                 GP_Coord x1, GP_Coord y1, GP_Pixel pixel)
+{
+	GP_CHECK_CONTEXT(context);
 
-  This is a main header for gfx part.
+	GP_FN_PER_BPP_CONTEXT(GP_Line_Raw, context, context, x0, y0, x1, y1,
+	                      pixel);
+}
+*/
 
- */
-
-#ifndef GP_GFX_H
-#define GP_GFX_H
-
-/* basic definitions and structures */
-#include "core/GP_Context.h"
-#include "core/GP_GetPutPixel.h"
-#include "core/GP_WritePixel.h"
-
-/* public drawing API */
-#include "GP_Fill.h"
-#include "GP_HLine.h"
-#include "GP_VLine.h"
-#include "GP_Line.h"
-#include "GP_Rect.h"
-#include "GP_Triangle.h"
-#include "GP_Tetragon.h"
-#include "GP_Circle.h"
-#include "GP_CircleSeg.h"
-#include "GP_Ellipse.h"
-#include "GP_Arc.h"
-#include "GP_Polygon.h"
-#include "GP_Symbol.h"
-
-#include "GP_PutPixelAA.h"
-#include "GP_LineAA.h"
-#include "GP_RectAA.h"
-
-#endif /* GP_GFX_H */
+void GP_LineAA(GP_Context *context, GP_Coord x0, GP_Coord y0,
+               GP_Coord x1, GP_Coord y1, GP_Pixel pixel)
+{
+	GP_CHECK_CONTEXT(context);
+	
+	GP_TRANSFORM_POINT_FP(context, x0, y0);
+	GP_TRANSFORM_POINT_FP(context, x1, y1);
+	
+	GP_LineAA_Raw(context, x0, y0, x1, y1, pixel);
+}
