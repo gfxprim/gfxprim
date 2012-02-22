1832d27ef46a9075db864ed0c5dee236ce1ac940
Tomas Gavenciak <gavento@ucw.cz>
2012-02-22 16:57:35 +0100

    Creating gfxprim module structure

---
+++ pylib/gfxprim/__init__.py
@@ -0,0 +1,17 @@
+"""
+GFXPrim Python bindings. Mostly generated using SWIG.
+
+Structure:
+gfxprim:
+  core:
+    context: module defining Context
+      Context - wrapper class, with many proxy methods
+    [Context from context]
+    gfxprim_core_c: - SWIG module
+    [almost * from gfxprim_core_c]
+  loaders:
+    gfxprim_loaders_c - SWIG submodule
+    [almost * from gfxprim_loaders_c]
+
+"""
+
