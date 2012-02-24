3b82597d1e2542af499e250d84f34d98721833e7
Tomas Gavenciak <gavento@ucw.cz>
2012-02-24 20:23:33 +0100

    pywrap: separate constants for gfx

--- pylib/gfxprim/gfx/__init__.py
+++ pylib/gfxprim/gfx/__init__.py
@@ -1,16 +1,25 @@
 from . import gfx_c
 
+from . import C
+
 def _init(module):
+
 
   # Imports from the SWIG module
   import re
   def strip_GP(s):
     return re.sub('^GP_', '', s)
 
-  # Import functions from the SWIG module
+  # Import constants from the SWIG module
   from ..utils import import_members
+  const_regexes = [
+      '^GP_[A-Z0-9_]*$',
+      '^GP_PIXEL_x[A-Z0-9_]*$']
+  import_members(gfx_c, C, include=const_regexes, sub=strip_GP)
+
+  # Import functions from the SWIG module
   import_members(gfx_c, module, sub=strip_GP,
-    exclude=[
+    exclude=const_regexes + [
       '.*_Raw',
       '^\w+_swigregister$',
       '^core_c$',
