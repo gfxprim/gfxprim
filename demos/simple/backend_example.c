db71e716da4792f972540c31b2d3d24aeede6825
Cyril Hrubis <metan@ucw.cz>
2012-05-05 19:30:31 +0200

    examples: Add a very simple examples.

---
+++ demos/simple/backend_example.c
@@ -0,0 +1,102 @@
+/*****************************************************************************
+ * This file is part of gfxprim library.                                     *
+ *                                                                           *
+ * Gfxprim is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                *
+ * License as published by the Free Software Foundation; either              *
+ * version 2.1 of the License, or (at your option) any later version.        *
+ *                                                                           *
+ * Gfxprim is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Lesser General Public License for more details.                           *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public          *
+ * License along with gfxprim; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                        *
+ * Boston, MA  02110-1301  USA                                               *
+ *                                                                           *
+ * Copyright (C) 2009-2012 Cyril Hrubis <metan@ucw.cz>                       *
+ *                                                                           *
+ *****************************************************************************/
+
+ /*
+
+   Simple backend example.
+
+  */
+
+#include <GP.h>
+#include <backends/GP_Backends.h>
+
+int main(int argc, char *argv[])
+{
+	GP_Backend *backend;
+	GP_Context *context;
+	GP_Pixel white_pixel, black_pixel;
+	const char *backend_opts = "X11:100x100";
+	int opt;
+
+	while ((opt = getopt(argc, argv, "b:h")) != -1) {
+		switch (opt) {
+		case 'b':
+			backend_opts = optarg;
+		break;
+		case 'h':
+			GP_BackendInit(NULL, NULL, stderr);
+			return 0;
+		break;
+		default:
+			fprintf(stderr, "Invalid paramter '%c'\n", opt);
+		}
+	}
+
+	/* Turn on debug messages */
+	GP_SetDebugLevel(10);
+
+	backend = GP_BackendInit(backend_opts, "Backend Example", stderr);
+
+	context = backend->context;
+
+	GP_EventSetScreenSize(context->w, context->h);
+	
+	black_pixel = GP_ColorToContextPixel(GP_COL_BLACK, context);
+	white_pixel = GP_ColorToContextPixel(GP_COL_WHITE, context);
+
+	GP_Fill(context, black_pixel);
+	GP_Line(context, 0, 0, context->w - 1, context->h - 1, white_pixel);
+	GP_Line(context, 0, context->h - 1, context->w - 1, 0, white_pixel);
+
+	/* Update the backend screen */
+	GP_BackendFlip(backend);
+
+	for (;;) {
+		if (backend->Poll)
+			GP_BackendPoll(backend);
+
+		usleep(1000);
+
+		/* Read and parse events */
+		GP_Event ev;
+
+		while (GP_EventGet(&ev)) {
+
+			GP_EventDump(&ev);
+			
+			switch (ev.type) {
+			case GP_EV_KEY:
+				switch (ev.val.key.key) {
+				case GP_KEY_ESC:
+				case GP_KEY_Q:
+					GP_BackendExit(backend);
+					return 0;
+				break;
+				}
+			}
+		}
+	}
+
+	GP_BackendExit(backend);
+
+	return 0;
+}
