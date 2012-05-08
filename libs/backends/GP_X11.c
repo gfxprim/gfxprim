179ad0f8718479d653783f56c3128ef4cad8d880
Cyril Hrubis <metan@ucw.cz>
2012-05-09 01:28:29 +0200

    backends: X11 fix off-by-one.

--- libs/backends/GP_X11.c
+++ libs/backends/GP_X11.c
@@ -69,7 +69,7 @@ static void x11_update_rect(GP_Backend *self, GP_Coord x0, GP_Coord y0,
 	XLockDisplay(x11->dpy);
 
 	XPutImage(x11->dpy, x11->win, DefaultGC(x11->dpy, x11->scr),
-	          x11->img, x0, y0, x0, y0, x1-x0, y1-y0);
+	          x11->img, x0, y0, x0, y0, x1-x0+1, y1-y0+1);
 	XFlush(x11->dpy);
 
 	x11->resized_flag = 0;
@@ -117,8 +117,8 @@ static void x11_poll(GP_Backend *self)
 				break;
 
 			x11_update_rect(self, ev.xexpose.x, ev.xexpose.y,
-			                ev.xexpose.x + ev.xexpose.width,
-					ev.xexpose.y + ev.xexpose.height);
+			                ev.xexpose.x + ev.xexpose.width - 1,
+					ev.xexpose.y + ev.xexpose.height - 1);
 		break;
 		case ConfigureNotify:
 			if (ev.xconfigure.width == (int)self->context->w &&
