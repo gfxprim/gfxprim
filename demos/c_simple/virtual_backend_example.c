1db9952e987f46349fe20c9d640cc7a322b2a2b0
Cyril Hrubis <metan@ucw.cz>
2012-05-21 13:18:17 +0200

    examples: Add virtual backend example.

--- demos/c_simple/virtual_backend_example.c
+++ demos/c_simple/virtual_backend_example.c
@@ -22,7 +22,10 @@
 
  /*
 
-   Simple backend example.
+   Simple virtual backend test.
+
+   Virtual backned allows you to test interactively pixel types that your
+   hardware/xserverd doesn't support.
 
   */
 
@@ -34,13 +37,22 @@ int main(int argc, char *argv[])
 	GP_Backend *backend;
 	GP_Context *context;
 	GP_Pixel white_pixel, black_pixel;
-	const char *backend_opts = "X11:100x100";
+	const char *backend_opts = "X11:400x400";
 	int opt;
+	GP_PixelType emul_type = GP_PIXEL_UNKNOWN;
 
-	while ((opt = getopt(argc, argv, "b:h")) != -1) {
+	while ((opt = getopt(argc, argv, "b:h:p:")) != -1) {
 		switch (opt) {
 		case 'b':
 			backend_opts = optarg;
+		break;
+		case 'p':
+			emul_type = GP_PixelTypeByName(optarg);
+
+			if (emul_type == GP_PIXEL_UNKNOWN) {
+				fprintf(stderr, "Invalid pixel type '%s'\n", optarg);
+				return 1;
+                        }
 		break;
 		case 'h':
 			GP_BackendInit(NULL, NULL, stderr);
@@ -55,7 +67,29 @@ int main(int argc, char *argv[])
 	/* Turn on debug messages */
 	GP_SetDebugLevel(10);
 
-	backend = GP_BackendInit(backend_opts, "Backend Example", stderr);
+	backend = GP_BackendInit(backend_opts, "Virtual Backend Example", stderr);
+
+	if (emul_type != GP_PIXEL_UNKNOWN) {
+		GP_Backend *emul;
+		
+		/* 
+		 * Create an emulated backend on the top of real backend.
+		 * 
+		 * The GP_BACKEND_CALL_EXIT says that when calling exit on
+		 * emulated backend, the real backend exit will be called as
+		 * well.
+		 */
+		emul = GP_BackendVirtualInit(backend, emul_type, GP_BACKEND_CALL_EXIT);
+
+		if (emul == NULL) {
+			fprintf(stderr, "Failed to create Virtual Backend\n");
+			GP_BackendExit(backend);
+			return 1;
+		}
+
+		/* Once created virtual backend behaves exactly like a real one */
+		backend = emul;
+	}
 
 	context = backend->context;
 
@@ -64,9 +98,23 @@ int main(int argc, char *argv[])
 	black_pixel = GP_ColorToContextPixel(GP_COL_BLACK, context);
 	white_pixel = GP_ColorToContextPixel(GP_COL_WHITE, context);
 
-	GP_Fill(context, black_pixel);
-	GP_Line(context, 0, 0, context->w - 1, context->h - 1, white_pixel);
-	GP_Line(context, 0, context->h - 1, context->w - 1, 0, white_pixel);
+	GP_Fill(context, white_pixel);
+
+	/* Now draw some testing patters */
+	unsigned int i;
+	for (i = 0; i < 40; i++) {
+		GP_HLineXYW(context, 0, i, i, black_pixel);
+		
+		GP_HLineXYW(context, 40 + i, i, i, black_pixel);
+		
+		GP_HLineXYW(context, 1, i + 40, i, black_pixel);
+		GP_HLineXYW(context, 2, i + 80, i, black_pixel);
+		GP_HLineXYW(context, 3, i + 120, i, black_pixel);
+		GP_HLineXYW(context, 4, i + 160, i, black_pixel);
+		GP_HLineXYW(context, 5, i + 200, i, black_pixel);
+		GP_HLineXYW(context, 6, i + 240, i, black_pixel);
+		GP_HLineXYW(context, 7, i + 280, i, black_pixel);
+	}
 
 	/* Update the backend screen */
 	GP_BackendFlip(backend);
