e09dcfd1600590e8bc57039293fbf39eba0d6fd2
Cyril Hrubis <metan@ucw.cz>
2012-09-14 16:42:54 +0200

    loaders: Simplify the image format detection code.

--- libs/loaders/GP_Loaders.c
+++ libs/loaders/GP_Loaders.c
@@ -34,9 +34,107 @@
 
 #include "GP_Loaders.h"
 
+enum GP_ImageFmt {
+	GP_FMT_UNKNOWN,
+	GP_FMT_PNG,
+	GP_FMT_JPG,
+	GP_FMT_BMP,
+	GP_FMT_GIF,
+	GP_FMT_PBM,
+	GP_FMT_PGM,
+	GP_FMT_PPM,
+};
+
+enum GP_ImageFmt filename_to_fmt(const char *path)
+{
+	size_t len = strlen(path);
+
+	if (len < 3)
+		return GP_FMT_UNKNOWN;
+
+	switch (path[len - 1]) {
+	/* PNG, JPG, JPEG */
+	case 'g':
+	case 'G':
+		switch (path[len - 2]) {
+		case 'n':
+		case 'N':
+			if (path[len - 3] == 'p' ||
+			    path[len - 3] == 'P')
+				return GP_FMT_PNG;
+		break;
+		case 'p':
+		case 'P':
+			if (path[len - 3] == 'j' ||
+			    path[len - 3] == 'J')
+				return GP_FMT_JPG;
+		break;
+		case 'e':
+		case 'E':
+			if ((path[len - 3] == 'p' ||
+			     path[len - 3] == 'P') &&
+			    (path[len - 4] == 'j' ||
+			     path[len - 4] == 'J'))
+				return GP_FMT_JPG;
+		break;
+		}
+	break;
+	/* PPM, PGM, PBM, PNM */
+	case 'm':
+	case 'M':
+		switch (path[len - 2]) {
+		case 'b':
+		case 'B':
+			if (path[len - 3] == 'p' ||
+			    path[len - 3] == 'P')
+				return GP_FMT_PBM;
+		break;
+		case 'g':
+		case 'G':
+			if (path[len - 3] == 'p' ||
+			    path[len - 3] == 'P')
+				return GP_FMT_PGM;
+		break;
+		case 'p':
+		case 'P':
+			if (path[len - 3] == 'p' ||
+			    path[len - 3] == 'P')
+				return GP_FMT_PPM;
+		break;
+		}
+	break;
+	/* BMP */
+	case 'P':
+	case 'p':
+		switch (path[len - 2]) {
+		case 'M':
+		case 'm':
+			if (path[len - 3] == 'B' ||
+			    path[len - 3] == 'b')
+				return GP_FMT_BMP;
+		break;
+		}
+	break;
+	/* GIF */
+	case 'F':
+	case 'f':
+		switch (path[len - 2]) {
+		case 'I':
+		case 'i':
+			if (path[len - 3] == 'G' ||
+			    path[len - 3] == 'g')
+				return GP_FMT_GIF;
+		break;
+		}
+	break;
+	}
+
+	return GP_FMT_UNKNOWN;
+}
+
 GP_Context *GP_LoadImage(const char *src_path, GP_ProgressCallback *callback)
 {
-	int len, saved_errno;
+	int saved_errno;
 	
 	if (access(src_path, R_OK)) {
 		
@@ -50,92 +148,27 @@ GP_Context *GP_LoadImage(const char *src_path, GP_ProgressCallback *callback)
 		return NULL;
 	}
 	
-	len = strlen(src_path);
+	enum GP_ImageFmt fmt = filename_to_fmt(src_path);
 
-	if (len < 3)
-		goto skip_filename_check;
-
-	switch (src_path[len - 1]) {
-	/* PNG, JPG, JPEG */
-	case 'g':
-	case 'G':
-		switch (src_path[len - 2]) {
-		case 'n':
-		case 'N':
-			if (src_path[len - 3] == 'p' ||
-			    src_path[len - 3] == 'P')
-				return GP_LoadPNG(src_path, callback);
-		break;
-		case 'p':
-		case 'P':
-			if (src_path[len - 3] == 'j' ||
-			    src_path[len - 3] == 'J')
-				return GP_LoadJPG(src_path, callback);
-		break;
-		case 'e':
-		case 'E':
-			if ((src_path[len - 3] == 'p' ||
-			     src_path[len - 3] == 'P') &&
-			    (src_path[len - 4] == 'j' ||
-			     src_path[len - 4] == 'J'))
-				return GP_LoadJPG(src_path, callback);
-		break;
-		}
-	break;
-	/* PPM, PGM, PBM, PNM */
-	case 'm':
-	case 'M':
-		switch (src_path[len - 2]) {
-		case 'b':
-		case 'B':
-			if (src_path[len - 3] == 'p' ||
-			    src_path[len - 3] == 'P') {
-				return GP_LoadPBM(src_path, callback);
-			}
-		break;
-		case 'g':
-		case 'G':
-			if (src_path[len - 3] == 'p' ||
-			    src_path[len - 3] == 'P') {
-				return GP_LoadPGM(src_path, callback);
-			}
-		break;
-		case 'p':
-		case 'P':
-			if (src_path[len - 3] == 'p' ||
-			    src_path[len - 3] == 'P') {
-				return GP_LoadPPM(src_path, callback);
-			}
-		break;
-		}
-	break;
-	/* BMP */
-	case 'P':
-	case 'p':
-		switch (src_path[len - 2]) {
-		case 'M':
-		case 'm':
-			if (src_path[len - 3] == 'B' ||
-			    src_path[len - 3] == 'b')
-				return GP_LoadBMP(src_path, callback);
-		break;
-		}
-	break;
-	/* GIF */
-	case 'F':
-	case 'f':
-		switch (src_path[len - 2]) {
-		case 'I':
-		case 'i':
-			if (src_path[len - 3] == 'G' ||
-			    src_path[len - 3] == 'g')
-				return GP_LoadGIF(src_path, callback);
-		break;
-		}
+	switch (fmt) {
+	case GP_FMT_JPG:
+		return GP_LoadJPG(src_path, callback);
+	case GP_FMT_PNG:
+		return GP_LoadPNG(src_path, callback);
+	case GP_FMT_GIF:
+		return GP_LoadGIF(src_path, callback);
+	case GP_FMT_BMP:
+		return GP_LoadGIF(src_path, callback);
+	case GP_FMT_PBM:
+		return GP_LoadPBM(src_path, callback);
+	case GP_FMT_PGM:
+		return GP_LoadPGM(src_path, callback);
+	case GP_FMT_PPM:
+		return GP_LoadPPM(src_path, callback);
+	
+	case GP_FMT_UNKNOWN:
 	break;
 	}
-
-skip_filename_check:
 
 	//TODO file signature based check
 	errno = ENOSYS;
@@ -144,41 +177,29 @@ skip_filename_check:
 
 int GP_LoadMetaData(const char *src_path, GP_MetaData *data)
 {
-	int len;
+	int saved_errno;
 	
-	len = strlen(src_path);
+	if (access(src_path, R_OK)) {
+		
+		saved_errno = errno;
 
-	if (len < 3) {
-		errno = ENOSYS;
+		GP_DEBUG(1, "Failed to access file '%s' : %s",
+		            src_path, strerror(errno));
+		
+		errno = saved_errno;
+		
 		return 1;
 	}
 
-	switch (src_path[len - 1]) {
-	/* PNG, JPG, JPEG */
-	case 'g':
-	case 'G':
-		switch (src_path[len - 2]) {
-		case 'n':
-		case 'N':
-			if (src_path[len - 3] == 'p' ||
-			    src_path[len - 3] == 'P')
-				return GP_LoadPNGMetaData(src_path, data); 
-		break;
-		case 'p':
-		case 'P':
-			if (src_path[len - 3] == 'j' ||
-			    src_path[len - 3] == 'J')
-				return GP_LoadJPGMetaData(src_path, data); 
-		break;
-		case 'e':
-		case 'E':
-			if ((src_path[len - 3] == 'p' ||
-			     src_path[len - 3] == 'P') &&
-			    (src_path[len - 4] == 'j' ||
-			     src_path[len - 4] == 'J'))
-				return GP_LoadJPGMetaData(src_path, data); 
-		break;
-		}
+	enum GP_ImageFmt fmt = filename_to_fmt(src_path);
+
+	switch (fmt) {
+	case GP_FMT_JPG:
+		return GP_LoadJPGMetaData(src_path, data);
+	case GP_FMT_PNG:
+		return GP_LoadPNGMetaData(src_path, data);
+	
+	default:
 	break;
 	}
 
@@ -189,41 +210,15 @@ int GP_LoadMetaData(const char *src_path, GP_MetaData *data)
 int GP_SaveImage(const GP_Context *src, const char *dst_path,
                  GP_ProgressCallback *callback)
 {
-	int len;
-	
-	len = strlen(dst_path);
+	enum GP_ImageFmt fmt = filename_to_fmt(dst_path);
 
-	if (len < 3) {
-		errno = ENOSYS;
-		return 1;
-	}
+	switch (fmt) {
+	case GP_FMT_JPG:
+		return GP_SaveJPG(src, dst_path, callback);
+	case GP_FMT_PNG:
+		return GP_SavePNG(src, dst_path, callback);
 
-	switch (dst_path[len - 1]) {
-	/* PNG, JPG, JPEG */
-	case 'g':
-	case 'G':
-		switch (dst_path[len - 2]) {
-		case 'n':
-		case 'N':
-			if (dst_path[len - 3] == 'p' ||
-			    dst_path[len - 3] == 'P')
-				return GP_SavePNG(src, dst_path, callback);
-		break;
-		case 'p':
-		case 'P':
-			if (dst_path[len - 3] == 'j' ||
-			    dst_path[len - 3] == 'J')
-				return GP_SaveJPG(src, dst_path, callback);
-		break;
-		case 'e':
-		case 'E':
-			if ((dst_path[len - 3] == 'p' ||
-			     dst_path[len - 3] == 'P') &&
-			    (dst_path[len - 4] == 'j' ||
-			     dst_path[len - 4] == 'J'))
-				return GP_SaveJPG(src, dst_path, callback);
-		break;
-		}
+	default:
 	break;
 	}
 
