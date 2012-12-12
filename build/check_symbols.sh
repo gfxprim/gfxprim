e77a976b35f4bec8d5d2f95de598e4944870bf8b
Cyril Hrubis <metan@ucw.cz>
2012-12-12 01:27:41 +0100

    build: Edhance exported symbols detection.

--- build/check_symbols.sh
+++ build/check_symbols.sh
@@ -8,27 +8,28 @@ SYMBOLFILE=libGP_symbols.txt
 function grep_sym
 {
 	if objdump --syms "$1" |grep "$2" 2>&1 > /dev/null; then
-		echo "SYM: $2 : $1"
+		echo -e "\t$1"
 	fi
 }
 
 function find_symbol
 {
+	echo "SYM $1:"
+	
 	find ../libs/ -name '*.o' | while read obj; do grep_sym "$obj" "$1"; done
 }
 
 objdump --dynamic-syms libGP.so | awk 'NR > 4 { print }' | awk '$3 != "*UND*"' | awk '{print $NF}'  > $SYMBOLFILE
 
 #
-# Remove a few known symbols
+# Remove compiler and glibc symbols
 #
-sed -i '/.init/d' $SYMBOLFILE
-sed -i '/_init/d' $SYMBOLFILE
-sed -i '/_end/d' $SYMBOLFILE
-sed -i '/_fini/d' $SYMBOLFILE
+sed -i '/^.init$/d' $SYMBOLFILE
+sed -i '/^__.*$/d' $SYMBOLFILE
+sed -i '/^_.*$/d' $SYMBOLFILE
 
 for i in `cat libGP_symbols.txt`; do
-	if ! grep "$i" libGP_expected_symbols.txt 2>&1 > /dev/null; then
+	if ! grep "$i" syms/*.txt 2>&1 > /dev/null; then
 		find_symbol "$i"
 	fi
 done
