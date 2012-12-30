f65e8ed5951cb097f67d212da67b48604c47bd66
Cyril Hrubis <metan@ucw.cz>
2012-12-30 12:29:49 +0100

    gfx: HLine: Split HLine && cleanup.
    This commit splits HLine implementation
    and remove unused files.

--- libs/gfx/GP_HLine.c
+++ libs/gfx/GP_HLine.c
@@ -23,43 +23,52 @@
  *                                                                           *
  *****************************************************************************/
 
-%% extends "base.c.t"
-
-{% block descr %}Horizontal line drawing{% endblock %}
-
-%% block body
-
-#include "core/GP_WritePixel.h"
+#include "core/GP_Transform.h"
 
 #include "gfx/GP_HLine.h"
+#include "gfx/GP_VLine.h"
 
-%% for ps in pixelsizes
-
-void GP_HLine_Raw_{{ ps.suffix }}(GP_Context *context, int x0, int x1, int y,
-			       GP_Pixel pixel)
+void GP_HLineXXY_Raw(GP_Context *context, GP_Coord x0, GP_Coord x1,
+                     GP_Coord y, GP_Pixel pixel)
 {
-	/* draw always from left to right, swap coords if necessary */
-	if (x0 > x1)
-		GP_SWAP(x0, x1);
+	GP_CHECK_CONTEXT(context);
 
-	/* return immediately if the line is completely out of surface */
-	if (y < 0 || y >= (int) context->h || x1 < 0 || x0 >= (int) context->w)
-		return;
-
-	/* clip the line against surface boundaries */
-	x0 = GP_MAX(x0, 0);
-	x1 = GP_MIN(x1, (int) context->w - 1);
-
-	size_t length = 1 + x1 - x0;
-	void *start = GP_PIXEL_ADDR(context, x0, y);
-
-%% if ps.needs_bit_endian()
-	GP_WritePixels_{{ ps.suffix }}(start, x0 % (8 / context->bpp), length, pixel);
-%% else
-	GP_WritePixels_{{ ps.suffix }}(start, length, pixel);
-%% endif
-
+	GP_FN_PER_BPP_CONTEXT(GP_HLine_Raw, context, context, x0, x1, y,
+	                      pixel);
 }
 
-%% endfor
-%% endblock body
+void GP_HLineXYW_Raw(GP_Context *context, GP_Coord x, GP_Coord y, GP_Size w,
+                     GP_Pixel pixel)
+{
+	if (w == 0)
+		return;
+
+	GP_HLineXXY_Raw(context, x, x + w - 1, y, pixel);
+}
+
+void GP_HLineXXY(GP_Context *context, GP_Coord x0, GP_Coord x1, GP_Coord y,
+                     GP_Pixel pixel)
+{
+	GP_CHECK_CONTEXT(context);
+
+	if (context->axes_swap) {
+		GP_TRANSFORM_Y(context, x0);
+		GP_TRANSFORM_Y(context, x1);
+		GP_TRANSFORM_X(context, y);
+		GP_VLine_Raw(context, y, x0, x1, pixel);
+	} else {
+		GP_TRANSFORM_X(context, x0);
+		GP_TRANSFORM_X(context, x1);
+		GP_TRANSFORM_Y(context, y);
+		GP_HLine_Raw(context, x0, x1, y, pixel);
+	}
+}
+
+void GP_HLineXYW(GP_Context *context, GP_Coord x, GP_Coord y, GP_Size w,
+                 GP_Pixel pixel)
+{
+	if (w == 0)
+		return;
+
+	GP_HLineXXY(context, x, x + w - 1, y, pixel);
+}
