33fd79bc8b7affb6ba6c72092670f4821ea29979
Cyril Hrubis <metan@ucw.cz>
2013-04-06 20:22:26 +0200

    demos: py_simple: Add X11 multiple windows example.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- demos/py_simple/x11_windows.py
+++ demos/py_simple/x11_windows.py
@@ -6,9 +6,20 @@ import gfxprim.core as core
 import gfxprim.gfx as gfx
 import gfxprim.backends as backends
 import gfxprim.input as input
+import gfxprim.text as text
 
-def redraw(bk):
-    bk.context.gfx.Fill(bk.context.RGBToPixel(0, 0, 0))
+def redraw(bk, id):
+    c = bk.context
+
+    black = c.RGBToPixel(0, 0, 0)
+    white = c.RGBToPixel(0xff, 0xff, 0xff)
+
+    c.gfx.Fill(black)
+
+    align = text.C.ALIGN_CENTER | text.C.VALIGN_CENTER
+
+    c.text.Text(None, c.w//2, c.h//2, align, white, black, "{} - {}x{}".format(id, c.w, c.h))
+    
     bk.Flip()
 
 def parse_events(bk, id):
@@ -29,16 +40,19 @@ def parse_events(bk, id):
         elif (ev.type == input.EV_SYS):
            if (ev.code == input.EV_SYS_QUIT):
                sys.exit(0)
+           if (ev.code == input.EV_SYS_RESIZE):
+               bk.ResizeAck()
+               redraw(bk, id)
 
 def main():
     # Create X11 windows
-    win1 = backends.BackendX11Init(None, 0, 0, 100, 100, "Win 1", 0)
-    win2 = backends.BackendX11Init(None, 0, 0, 100, 100, "Win 2", 0)
+    win1 = backends.BackendX11Init(None, 0, 0, 200, 100, "Win 1", 0)
+    win2 = backends.BackendX11Init(None, 0, 0, 200, 100, "Win 2", 0)
     assert(win1)
     assert(win2)
 
-    redraw(win1)
-    redraw(win2)
+    redraw(win1, "win1")
+    redraw(win2, "win2")
 
     # Event loop
     while True:
