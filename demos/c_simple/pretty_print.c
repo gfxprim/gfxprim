c02f7e5b2c18615e370b15581cd1b29c18a4bc79
Cyril Hrubis <metan@ucw.cz>
2013-06-12 22:15:16 +0200

    demos: c_simple: Add pretty print example.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ demos/c_simple/pretty_print.c
@@ -0,0 +1,43 @@
+/*****************************************************************************
+ * This file is part of gfxprim library.                                     *
+ *                                                                           *
+ * Gfxprim is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                *
+ * License as published by the Free Software Foundation; either              *
+ * version 2.1 of the License, or (at your option) any later version.        *
+ *                                                                           *
+ * Gfxprim is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Lesser General Public License for more details.                           *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public          *
+ * License along with gfxprim; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                        *
+ * Boston, MA  02110-1301  USA                                               *
+ *                                                                           *
+ * Copyright (C) 2009-2013 Cyril Hrubis <metan@ucw.cz>                       *
+ *                                                                           *
+ *****************************************************************************/
+
+/*
+ 
+  Pretty print function for pixel and context.
+
+ */
+
+#include <GP.h>
+
+int main(void)
+{
+	GP_Context *ctx = GP_ContextAlloc(100, 100, GP_PIXEL_RGB888);
+	GP_Pixel pix = ~(GP_Pixel)0;
+
+	/* Pretty prints pixel values */
+	GP_PixelPrint(pix, GP_PIXEL_RGB888);
+
+	/* Pretty prints context info */
+	GP_ContextPrintInfo(ctx);
+
+	return 0;
+}
