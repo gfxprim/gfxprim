d9a8b27f7b932cc340d5d0251ab292fe388216a7
Cyril Hrubis <metan@ucw.cz>
2013-06-19 00:15:03 +0200

    libs: loaders: GP_Loader.c: Cleanup.
    Get finally rid of the big piece of old code.
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- libs/loaders/GP_Loader.c
+++ libs/loaders/GP_Loader.c
@@ -204,10 +204,9 @@ static struct GP_Loader *loader_by_extension(const char *ext)
 	return NULL;
 }
 
-static struct GP_Loader *loader_by_filename(const char *path)
+static const char *get_ext(const char *path)
 {
 	size_t len = strlen(path);
-	const char *ext;
 	int i;
 
 	for (i = len - 1; i >= 0; i--)
@@ -217,7 +216,15 @@ static struct GP_Loader *loader_by_filename(const char *path)
 	if (path[i] != '.')
 		return NULL;
 
-	ext = path + i + 1;
+	return path + i + 1;
+}
+
+static struct GP_Loader *loader_by_filename(const char *path)
+{
+	const char *ext = get_ext(path);
+
+	if (ext == NULL)
+		return NULL;
 
 	GP_DEBUG(1, "Loading file by filename extension '%s'", ext);
 
@@ -265,18 +272,15 @@ err0:
 
 GP_Context *GP_LoadImage(const char *src_path, GP_ProgressCallback *callback)
 {
-	int saved_errno;
+	int err;
 	struct stat st;
 	
 	if (access(src_path, R_OK)) {
-		
-		saved_errno = errno;
-
+		err = errno;
 		GP_DEBUG(1, "Failed to access file '%s' : %s",
 		            src_path, strerror(errno));
 		
-		errno = saved_errno;
-		
+		errno = err;
 		return NULL;
 	}
 
@@ -322,139 +326,31 @@ GP_Context *GP_LoadImage(const char *src_path, GP_ProgressCallback *callback)
 	return NULL;
 }
 
-enum GP_ImageFmt {
-	GP_FMT_UNKNOWN,
-	GP_FMT_PNG,
-	GP_FMT_JPG,
-	GP_FMT_BMP,
-	GP_FMT_GIF,
-	GP_FMT_PSP,
-	GP_FMT_PBM,
-	GP_FMT_PGM,
-	GP_FMT_PPM,
-};
-
-static enum GP_ImageFmt filename_to_fmt(const char *path)
-{
-	size_t len = strlen(path);
-
-	if (len < 3)
-		return GP_FMT_UNKNOWN;
-
-	switch (path[len - 1]) {
-	/* PNG, JPG, JPEG */
-	case 'g':
-	case 'G':
-		switch (path[len - 2]) {
-		case 'n':
-		case 'N':
-			if (path[len - 3] == 'p' ||
-			    path[len - 3] == 'P')
-				return GP_FMT_PNG;
-		break;
-		case 'p':
-		case 'P':
-			if (path[len - 3] == 'j' ||
-			    path[len - 3] == 'J')
-				return GP_FMT_JPG;
-		break;
-		case 'e':
-		case 'E':
-			if ((path[len - 3] == 'p' ||
-			     path[len - 3] == 'P') &&
-			    (path[len - 4] == 'j' ||
-			     path[len - 4] == 'J'))
-				return GP_FMT_JPG;
-		break;
-		}
-	break;
-	/* PPM, PGM, PBM, PNM */
-	case 'm':
-	case 'M':
-		switch (path[len - 2]) {
-		case 'b':
-		case 'B':
-			if (path[len - 3] == 'p' ||
-			    path[len - 3] == 'P')
-				return GP_FMT_PBM;
-		break;
-		case 'g':
-		case 'G':
-			if (path[len - 3] == 'p' ||
-			    path[len - 3] == 'P')
-				return GP_FMT_PGM;
-		break;
-		case 'p':
-		case 'P':
-			if (path[len - 3] == 'p' ||
-			    path[len - 3] == 'P')
-				return GP_FMT_PPM;
-		break;
-		}
-	break;
-	/* BMP, PSP */
-	case 'P':
-	case 'p':
-		switch (path[len - 2]) {
-		case 'M':
-		case 'm':
-			if (path[len - 3] == 'B' ||
-			    path[len - 3] == 'b')
-				return GP_FMT_BMP;
-		break;
-		case 'S':
-		case 's':
-			if (path[len - 3] == 'P' ||
-			    path[len - 3] == 'p')
-				return GP_FMT_PSP;
-		break;
-		}
-	break;
-	/* GIF */
-	case 'F':
-	case 'f':
-		switch (path[len - 2]) {
-		case 'I':
-		case 'i':
-			if (path[len - 3] == 'G' ||
-			    path[len - 3] == 'g')
-				return GP_FMT_GIF;
-		break;
-		}
-	break;
-	}
-
-	return GP_FMT_UNKNOWN;
-}
-
 int GP_LoadMetaData(const char *src_path, GP_MetaData *data)
 {
-	int saved_errno;
-	
-	if (access(src_path, R_OK)) {
-		
-		saved_errno = errno;
+	const char *ext;
+	int err;
 
+	if (access(src_path, R_OK)) {
+		err = errno;
 		GP_DEBUG(1, "Failed to access file '%s' : %s",
 		            src_path, strerror(errno));
-		
-		errno = saved_errno;
-		
+		errno = err;
 		return 1;
 	}
 
-	enum GP_ImageFmt fmt = filename_to_fmt(src_path);
+	ext = get_ext(src_path);
 
-	switch (fmt) {
-	case GP_FMT_JPG:
+	if (ext == NULL)
+		goto out;
+
+	if (!strcasecmp(ext, "jpg") || !strcasecmp(ext, "jpeg"))
 		return GP_LoadJPGMetaData(src_path, data);
-	case GP_FMT_PNG:
-		return GP_LoadPNGMetaData(src_path, data);
 	
-	default:
-	break;
-	}
+	if (!strcasecmp(ext, "png"))
+		return GP_LoadPNGMetaData(src_path, data);
 
+out:
 	errno = ENOSYS;
 	return 1;
 }
