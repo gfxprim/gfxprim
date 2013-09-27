6983b8143445758e54e41dfa0ae8c779324aa1ed
Cyril Hrubis <metan@ucw.cz>
2013-09-27 22:56:08 +0200

    doc: filters: Rewrite the example image generator
    The example image generator is now generic, yay!
    
    * generate median images automatically
    
    * update the parent asciidoc docs accordingly
    
    * update python filters docs
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ doc/images/regen.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+
+import gfxprim.core as core
+import gfxprim.loaders as loaders
+import gfxprim.filters as filters
+
+class ImgGen:
+    def __init__(self, orig_path):
+        self.orig_path = orig_path
+        self.img = loaders.Load('../' + orig_path + 'lenna.png')
+        self.img_small = loaders.Load('../' + orig_path + 'lenna_small.png')
+
+
+    def write_asciidoc_head(self, dst_path, heading):
+        self.f = open('../' + dst_path + 'images.txt', 'w')
+        f = self.f
+        f.write('.Example Images: Original Image; ' + heading + '\n')
+        f.write('image:' + self.orig_path + 'lenna_small.png[\n')
+        f.write('\t"Original Image",\n')
+        f.write('\tlink="' + self.orig_path + 'lenna.png"]\n')
+
+    def write_img_asciidoc(self, desc, fname, fname_small):
+        f = self.f
+        f.write('image:' + fname_small + '[\n')
+        f.write('\t"' + desc + '",\n')
+        f.write('\tlink="' + fname + '"]\n')
+
+    def write_asciidoc_tail(self):
+        self.f.write('\n')
+        self.f.close()
+
+    def gen(self, func, func_params_arr, dst_path, func_name):
+
+        print("Generating " + func_name)
+
+        self.write_asciidoc_head(dst_path, func_name + ' ' +
+        ', '.join(map(lambda x: ' '.join([str(i) for i in x]), func_params_arr)))
+
+        for i in func_params_arr:
+            str_i = [str(x) for x in i]
+            desc = func_name + ' ' + ' '.join(str_i)
+
+            print(' > ' + desc)
+
+            fname = dst_path + 'lenna_' + '_'.join(str_i) + '.png'
+            fname_small = dst_path + 'lenna_small_' + '_'.join(str_i) + '.png'
+
+            self.write_img_asciidoc(desc, fname, fname_small)
+
+            res = func(self.img, *i)
+            res.loaders.Save('../' + fname)
+
+            res = res.filters.ResizeAlloc(self.img_small.w, self.img_small.h, 2)
+            res.loaders.Save('../' + fname_small)
+
+        self.write_asciidoc_tail()
+
+def main():
+    imggen = ImgGen('images/orig/')
+
+    imggen.gen(filters.GaussianBlurAlloc,
+               [[2, 2], [0, 4], [4, 0], [4, 4], [10, 10]],
+	       'images/blur/', 'Gaussian Blur')
+
+    imggen.gen(filters.MedianAlloc,
+               [[3, 3], [5, 5], [7, 7], [9, 9], [12, 12]],
+	       'images/median/', 'Median')
+
+if __name__ == '__main__':
+    main()
