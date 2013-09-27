ec015ac1774ec0b833b2c9206f924361f897e20d
Cyril Hrubis <metan@ucw.cz>
2013-09-27 23:52:23 +0200

    doc: filters: regen.py: Add names to the filter params.
    Now the example images description includes id= for each filter
    parameter value.
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- doc/images/regen.py
+++ doc/images/regen.py
@@ -16,7 +16,7 @@ class ImgGen:
     def write_asciidoc_head(self, dst_path, heading):
         self.f = open('../' + dst_path + 'images.txt', 'w')
         f = self.f
-        f.write('.Example Images: Original Image; ' + heading + '\n')
+        f.write('.Original Image; ' + heading + '\n')
         f.write('image:' + self.orig_path + 'lenna_small.png[\n')
         f.write('\t"Original Image",\n')
         f.write('\tlink="' + self.orig_path + 'lenna.png"]\n')
@@ -31,12 +31,13 @@ class ImgGen:
         self.f.write('\n')
         self.f.close()
 
-    def gen(self, func, func_params_arr, dst_path, func_name):
+    def gen(self, func, func_param_desc, func_params_arr, dst_path, func_name):
 
         print("Generating " + func_name)
 
         self.write_asciidoc_head(dst_path, func_name + ' ' +
-        ', '.join(map(lambda x: ' '.join([str(i) for i in x]), func_params_arr)))
+         ', '.join(map(lambda x: ' '.join([func_param_desc[i] + '=' +
+	 str(x[i]) for i in range(0, len(x))]), func_params_arr)))
 
         for i in func_params_arr:
             str_i = [str(x) for x in i]
@@ -60,19 +61,19 @@ class ImgGen:
 def main():
     imggen = ImgGen('images/orig/')
 
-    imggen.gen(filters.GaussianBlurAlloc,
+    imggen.gen(filters.GaussianBlurAlloc, ['xsig', 'ysig'],
                [[2, 2], [0, 4], [4, 0], [4, 4], [10, 10]],
 	       'images/blur/', 'Gaussian Blur')
 
-    imggen.gen(filters.MedianAlloc,
+    imggen.gen(filters.MedianAlloc, ['xr', 'yr'],
                [[3, 3], [5, 5], [7, 7], [9, 9], [12, 12]],
 	       'images/median/', 'Median')
 
-    imggen.gen(filters.EdgeSharpeningAlloc,
+    imggen.gen(filters.EdgeSharpeningAlloc, ['w'],
                [[0.1], [0.3], [0.5], [0.8], [1.0]],
 	       'images/edge_sharpening/', 'Edge Sharpening')
 
-    imggen.gen(filters.GaussianNoiseAddAlloc,
+    imggen.gen(filters.GaussianNoiseAddAlloc, ['s', 'm'],
                [[0.03, 0], [0.05, 0], [0.05, -0.1], [0.05, 0.1], [0.07, 0.0]],
 	       'images/gaussian_noise/', 'Gaussian Additive Noise')
 
