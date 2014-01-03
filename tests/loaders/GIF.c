342537c6df7ca58ca4ac35d0b6f1dae16ef6746a
Cyril Hrubis <metan@ucw.cz>
2014-01-03 20:48:18 +0100

    tests: loaders: Very basic GIF test.
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ tests/loaders/GIF.c
@@ -0,0 +1,68 @@
+/*****************************************************************************
+ * This file is part of gfxprim library.                                     *
+ *                                                                           *
+ * Gfxprim is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                *
+ * License as published by the Free Software Foundation; either              *
+ * version 2.1 of the License, or (at your option) any later version.        *
+ *                                                                           *
+ * Gfxprim is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Lesser General Public License for more details.                           *
+ *                                                                           *
+ * You should have received a copy of the GNU Lesser General Public          *
+ * License along with gfxprim; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,                        *
+ * Boston, MA  02110-1301  USA                                               *
+ *                                                                           *
+ * Copyright (C) 2009-2014 Cyril Hrubis <metan@ucw.cz>                       *
+ *                                                                           *
+ *****************************************************************************/
+
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include <core/GP_Context.h>
+#include <core/GP_GetPutPixel.h>
+#include <loaders/GP_Loaders.h>
+
+#include "tst_test.h"
+
+static int test_load_GIF(const char *path)
+{
+	GP_Context *img;
+
+	errno = 0;
+
+	img = GP_LoadGIF(path, NULL);
+
+	if (img == NULL) {
+		switch (errno) {
+		case ENOSYS:
+			tst_msg("Not Implemented");
+			return TST_SKIPPED;
+		default:
+			tst_msg("Got %s", strerror(errno));
+			return TST_FAILED;
+		}
+	}
+
+	GP_ContextFree(img);
+
+	return TST_SUCCESS;
+}
+
+const struct tst_suite tst_suite = {
+	.suite_name = "GIF",
+	.tests = {
+		{.name = "GIF Load 100x100",
+		 .tst_fn = test_load_GIF,
+		 .res_path = "data/gif/valid/100x100-white.gif",
+		 .data = "100x100-white.gif",
+		 .flags = TST_TMPDIR | TST_CHECK_MALLOC},
+
+		{.name = NULL},
+	}
+};
