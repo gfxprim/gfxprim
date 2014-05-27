8c85a6820eef1c67fef1d2ca545eaaa478844c85
Cyril Hrubis <metan@ucw.cz>
2014-05-27 21:59:35 +0200

    demos/py_simple: New python gfxprim + Qt example
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ demos/py_simple/gfxprim_qt.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+from PySide import QtGui
+import gfxprim.core as core
+import gfxprim.loaders as loaders
+
+def getpixmap(path):
+    img = loaders.Load(path)
+    if img.pixel_type != core.C.PIXEL_BGR888:
+        img = img.Convert(core.C.PIXEL_BGR888)
+    qt_img = QtGui.QImage(img.ToByteArray(), img.w, img.h,
+                          img.bytes_per_row, QtGui.QImage.Format_RGB888)
+    pix = QtGui.QPixmap.fromImage(qt_img)
+    return pix
+
+class ImageLabel(QtGui.QLabel):
+    def __init__(self, path, parent=None):
+        QtGui.QLabel.__init__(self, parent)
+
+        self.setWindowTitle(path)
+
+        self.pix = getpixmap(path)
+        self.setPixmap(self.pix)
+        size = self.pix.size()
+        self.setGeometry(100, 100, size.width(), size.height())
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    w = ImageLabel(sys.argv[1])
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
