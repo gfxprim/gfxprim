1fefd006f1c71001c958f86b9f1e58d92dac7363
Cyril Hrubis <metan@ucw.cz>
2020-03-22 20:25:22 +0100

    utils: Add proper vector test + example
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

---
+++ demos/c_simple/vector.c
@@ -0,0 +1,98 @@
+//SPDX-License-Identifier: LGPL-2.0-or-later
+/*
+
+  Copyright (C) 2020 Richard Palethorpe <richiejp@f-m.fm>
+  Copyright (C) 2020 Cyril Hrubis <metan@ucw.cz>
+
+*/
+
+#include <stdio.h>
+#include <utils/gp_vec.h>
+
+static void print_int_vec(int *payload)
+{
+	size_t i;
+	gp_vec *vec = GP_VEC(payload);
+
+	printf("gp_vec { .unit = %zu, .capacity = %zu, .length = %zu, .payload = [\n ",
+	       vec->unit, vec->capacity, vec->length);
+
+	for (i = 0; i < vec->length; i++)
+		printf("\t%zu\t= %d\n", i, payload[i]);
+
+	if (vec->length < vec->capacity)
+		printf("\t... +%zu unused capacity\n", vec->capacity - vec->length);
+
+	printf("]}\n");
+}
+
+int main(void)
+{
+	size_t i;
+	int *ints = gp_vec_new(10, sizeof(int));
+	char **strs;
+
+	for (i = 0; i < gp_vec_len(ints); i++)
+		ints[i] = i + 1;
+
+	printf("Initial range (1,10): ");
+	print_int_vec(ints);
+
+	ints = gp_vec_insert(ints, 0, 1);
+	ints[0] = 0;
+
+	printf("\nInsert 0: ");
+	print_int_vec(ints);
+
+	ints = GP_VEC_PUSH(ints, 11);
+
+	printf("\nPush 11: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_insert(ints, 4, 1);
+	ints[4] = -1;
+
+	printf("\nInsert -1 at 4: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_insert(ints, 5, 3);
+
+	printf("\nInsert 3 of 0 at 5: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_delete(ints, 5, 3);
+	printf("\nDelete 3 of 0 at 5: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_delete(ints, 4, 1);
+	printf("\nDelete -1 at 4: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_delete(ints, 11, 1);
+	printf("\nDelete 11 at 11: ");
+	print_int_vec(ints);
+
+	ints = gp_vec_delete(ints, 0, 1);
+	printf("\nDelete 0 at 0: ");
+	print_int_vec(ints);
+
+	gp_vec_free(ints);
+
+	strs = gp_vec_new(10, sizeof(char*));
+
+	for (i = 0; i < gp_vec_len(strs); i++) {
+		strs[i] = gp_vec_new(10, sizeof(char));
+		snprintf(strs[i], gp_vec_len(strs[i]), "Line %zu", i);
+	}
+
+	printf("\nVec of string vecs: \n");
+	for (i = 0; i < gp_vec_len(strs); i++)
+		printf("%s\n", strs[i]);
+
+	for (i = 0; i < gp_vec_len(strs); i++)
+		gp_vec_free(strs[i]);
+
+	gp_vec_free(strs);
+
+	return 0;
+}
---
+++ tests/utils/vec.c
@@ -0,0 +1,244 @@
+// SPDX-License-Identifier: GPL-2.1-or-later
+/*
+
+  Copyright (C) 2020 Richard Palethorpe <richiejp@f-m.fm>
+  Copyright (C) 2009-2020 Cyril Hrubis <metan@ucw.cz>
+
+ */
+
+#include <string.h>
+#include <errno.h>
+
+#include <utils/gp_vec.h>
+
+#include "tst_test.h"
+
+static void print_int_vec(int *payload)
+{
+	size_t i;
+	gp_vec *vec = GP_VEC(payload);
+
+	printf("gp_vec { .unit = %zu, .capacity = %zu, .length = %zu, .payload = [\n ",
+	       vec->unit, vec->capacity, vec->length);
+
+	for (i = 0; i < vec->length; i++)
+		printf("\t%zu\t= %d\n", i, payload[i]);
+
+	if (vec->length < vec->capacity)
+		printf("\t... +%zu unused capacity\n", vec->capacity - vec->length);
+
+	printf("]}\n");
+}
+
+static int test_vec(void)
+{
+	size_t i;
+	int *vec = gp_vec_new(10, sizeof(int));
+
+	if (!vec) {
+		tst_err("Failed to allocate vector");
+		return TST_UNTESTED;
+	}
+
+	if (gp_vec_len(vec) != 10) {
+		tst_msg("Invalid vector lenght");
+		return TST_FAILED;
+	}
+
+	for (i = 0; i < gp_vec_len(vec); i++) {
+		if (vec[i] != 0) {
+			tst_msg("Vector not zeroed!");
+			return TST_FAILED;
+		}
+	}
+
+	gp_vec_free(vec);
+
+	return TST_SUCCESS;
+}
+
+static int test_vec_free(void)
+{
+	gp_vec_free(NULL);
+
+	return TST_SUCCESS;
+}
+
+static int test_vec_zero_size(void)
+{
+	int *vec = gp_vec_new(0, sizeof(int));
+
+	if (gp_vec_len(vec) != 0) {
+		tst_err("Nonzero vector size");
+		return TST_FAILED;
+	}
+
+	gp_vec_free(vec);
+
+	return TST_SUCCESS;
+}
+
+struct insert_test {
+	size_t len;
+
+	ssize_t off;
+	size_t cnt;
+
+	int fail;
+};
+
+static int test_vec_insert(struct insert_test *tst)
+{
+	size_t i;
+	int *vec = gp_vec_new(tst->len, sizeof(int));
+
+	if (!vec) {
+		tst_err("Failed to allocate vector");
+		return TST_UNTESTED;
+	}
+
+	if (gp_vec_len(vec) != tst->len) {
+		tst_msg("Invalid vector lenght");
+		return TST_FAILED;
+	}
+
+	for (i = 0; i < gp_vec_len(vec); i++)
+		vec[i] = i;
+
+	int *new_vec = gp_vec_insert(vec, tst->off, tst->cnt);
+
+	if (tst->fail) {
+		if (new_vec) {
+			tst_msg("Insert should have failed!");
+			return TST_FAILED;
+		}
+
+		gp_vec_free(vec);
+		return TST_SUCCESS;
+	}
+
+	if (!new_vec) {
+		tst_msg("Insert failed");
+		return TST_FAILED;
+	}
+
+	vec = new_vec;
+
+	if (gp_vec_len(vec) != tst->len + tst->cnt) {
+		tst_msg("Invalid vector lenght after insert %zu expected %zu",
+			gp_vec_len(vec), tst->len + tst->cnt);
+		return TST_FAILED;
+	}
+
+	for (i = 0; i < tst->cnt; i++) {
+		if (vec[tst->off + i]) {
+			tst_msg("Inserted space not zeroed");
+			print_int_vec(vec);
+			return TST_FAILED;
+		}
+	}
+
+	vec = gp_vec_delete(vec, tst->off, tst->cnt);
+
+	if (!vec) {
+		tst_msg("Delete failed");
+		return TST_FAILED;
+	}
+
+	if (gp_vec_len(vec) != tst->len) {
+		tst_msg("Invalid vector lenght after delete %zu expected %zu",
+			gp_vec_len(vec), tst->len);
+		return TST_FAILED;
+	}
+
+	for (i = 0; i < gp_vec_len(vec); i++) {
+		if (vec[i] != (int)i) {
+			tst_msg("Wrong data in vector");
+			print_int_vec(vec);
+			return TST_FAILED;
+		}
+	}
+
+	gp_vec_free(vec);
+
+	return TST_SUCCESS;
+}
+
+static struct insert_test test1 = {
+	.len = 1,
+	.off = 0,
+	.cnt = 3,
+	.fail = 0,
+};
+
+static struct insert_test test2 = {
+	.len = 1,
+	.off = 1,
+	.cnt = 7,
+	.fail = 0,
+};
+
+static struct insert_test test3 = {
+	.len = 2,
+	.off = 1,
+	.cnt = 5,
+	.fail = 0,
+};
+
+static struct insert_test test4 = {
+	.len = 2,
+	.off = 1,
+	.cnt = 1024,
+	.fail = 0,
+};
+
+static struct insert_test test_fail1 = {
+	.len = 2,
+	.off = 3,
+	.cnt = 5,
+	.fail = 1,
+};
+
+const struct tst_suite tst_suite = {
+	.suite_name = "vector testsuite",
+	.tests = {
+		{.name = "basic test",
+		 .tst_fn = test_vec,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "vector free on NULL",
+		 .tst_fn = test_vec_free,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "vector zero size",
+		 .tst_fn = test_vec_zero_size,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "insert test start",
+		 .tst_fn = test_vec_insert,
+		 .data = &test1,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "insert test end",
+		 .tst_fn = test_vec_insert,
+		 .data = &test2,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "insert test middle",
+		 .tst_fn = test_vec_insert,
+		 .data = &test3,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "insert test huge",
+		 .tst_fn = test_vec_insert,
+		 .data = &test4,
+		 .flags = TST_CHECK_MALLOC},
+
+		{.name = "insert test after end",
+		 .tst_fn = test_vec_insert,
+		 .data = &test_fail1,
+		 .flags = TST_CHECK_MALLOC},
+
+		{}
+	}
+};
