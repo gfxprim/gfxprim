42de55b4a16a61afc38d87d8af42da5c9ec52b4e
Cyril Hrubis <metan@ucw.cz>
2020-12-21 16:43:36 +0100

    install.mk: Install man pages without executable flag
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- install.mk
+++ install.mk
@@ -25,11 +25,11 @@ endif
 ifdef INSTALL_MAN
 ifdef VERBOSE
 	install -d "$(MAN_DIR)"
-	for i in $(INSTALL_MAN); do install "$$i" "$(MAN_DIR)"; done
+	for i in $(INSTALL_MAN); do install -m 644 "$$i" "$(MAN_DIR)"; done
 else
 	@install -d "$(MAN_DIR)"
 	@echo "MKDIR $(MAN_DIR)"
-	@for i in $(INSTALL_MAN); do echo "CP    $$i $(MAN_DIR)"; install "$$i" "$(MAN_DIR)"; done
+	@for i in $(INSTALL_MAN); do echo "CP    $$i $(MAN_DIR)"; install -m 644 "$$i" "$(MAN_DIR)"; done
 endif
 endif
 
