02ec6e7f4e86f48538bb10fb66a36b747de12560
Cyril Hrubis <metan@ucw.cz>
2021-01-14 20:56:07 +0100

    widgets: demos: shoimage
    Do not segfault when image is missing.
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- demos/widgets/showimage.c
+++ demos/widgets/showimage.c
@@ -29,7 +29,8 @@ static void alloc_and_blit(gp_widget_event *ev)
 
 	gp_pixmap *p = gp_pixmap_alloc(w->w, w->h, ev->ctx->pixel_type);
 
-	gp_blit_xywh(image, 0, 0, p->w, p->h, p, 0, 0);
+	if (image)
+		gp_blit_xywh(image, 0, 0, p->w, p->h, p, 0, 0);
 
 	gp_pixmap_free(w->pixmap->pixmap);
 	w->pixmap->pixmap = p;
@@ -53,6 +54,7 @@ int main(int argc, char *argv[])
 {
 	gp_widget *layout = gp_widget_grid_new(1, 1);
 	gp_widget *area = gp_widget_scroll_area_new(320, 240, NULL);
+	gp_size w = 320, h = 240;
 
 	layout->align = GP_FILL;
 	area->align = GP_FILL;
@@ -61,10 +63,15 @@ int main(int argc, char *argv[])
 
 	gp_widget_grid_put(layout, 0, 0, area);
 
-	if (argv[0])
+	if (argv[0]) {
 		image = gp_load_image(argv[0], NULL);
+		if (image) {
+			w = image->w;
+			h = image->h;
+		}
+	}
 
-	gp_widget *pixmap = gp_widget_pixmap_new(image->w, image->h, pixmap_on_event, NULL);
+	gp_widget *pixmap = gp_widget_pixmap_new(w, h, pixmap_on_event, NULL);
 
 	gp_widget_event_unmask(pixmap, GP_WIDGET_EVENT_REDRAW);
 
