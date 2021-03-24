67ed40066525866e74326b33ade52af7dc0a24f8
Cyril Hrubis <metan@ucw.cz>
2021-03-24 20:37:18 +0100

    tests: malloc cananries: use mmap()
    This simplifes the code a bit.
    
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- tests/framework/tst_malloc_canaries.c
+++ tests/framework/tst_malloc_canaries.c
@@ -14,14 +14,17 @@
 
 void *tst_malloc_canary_right(size_t size)
 {
-	size_t pagesize = sysconf(_SC_PAGESIZE);
+	size_t pagesize = getpagesize();
 	size_t pages = size/pagesize + !!(size%pagesize) + 1;
-	char *buf;
+	void *ret;
 
 	if (size == 0)
 		return NULL;
 
-	if (posix_memalign((void*)&buf, pagesize, pages * pagesize))
+	ret = mmap(NULL, pages * pagesize, PROT_READ | PROT_WRITE,
+	           MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
+	if (ret == MAP_FAILED)
 		return NULL;
 
 	/*
@@ -29,75 +32,60 @@ void *tst_malloc_canary_right(size_t size)
 	 * somewhere in the first page so that the end is exactly before the
 	 * start of the last page.
 	 */
-	if (mprotect(buf + pagesize * (pages - 1), pagesize, PROT_NONE)) {
+	if (mprotect(ret + (pages - 1) * pagesize, pagesize, PROT_NONE)) {
 		perror("mprotect");
-		free(buf);
+		munmap(ret, pages * pagesize);
 		return NULL;
 	}
 
-	if (size%pagesize)
-		return buf + (pagesize - size%pagesize);
+	if (size % pagesize)
+		return ret + (pagesize - size%pagesize);
 
-	return buf;
+	return ret;
 }
-
-static void (*real_free)(void *) = NULL;
 
 void tst_free_canary_right(void *ptr, size_t size)
 {
-	size_t pagesize = sysconf(_SC_PAGESIZE);
+	size_t pagesize = getpagesize();
 	size_t pages = size/pagesize + !!(size%pagesize);
-	void *start = size%pagesize ? (char*)ptr - (pagesize - size%pagesize) : ptr;
+	void *start = size%pagesize ? ptr - (pagesize - size%pagesize) : ptr;
 
-	/* Reset the memory protection back to RW */
-	if (mprotect(start + pagesize * pages, pagesize, PROT_READ | PROT_WRITE))
-		perror("mprotect");
-
-	if (!real_free)
-		real_free = dlsym(RTLD_NEXT, "free");
-
-	real_free(start);
+	munmap(start, pages * pagesize);
 }
 
 void *tst_malloc_canary_left(size_t size)
 {
 	size_t pagesize = sysconf(_SC_PAGESIZE);
 	size_t pages = size/pagesize + !!(size%pagesize) + 1;
+	void *ret;
 
 	if (size == 0)
 		return NULL;
 
-	char *buf;
+	ret = mmap(NULL, pages * pagesize, PROT_READ | PROT_WRITE,
+	           MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
-	if (posix_memalign((void*)&buf, pagesize, pages * pagesize))
+	if (ret == MAP_FAILED)
 		return NULL;
 
 	/*
 	 * Turn off read/write acces on the first page, the buffer starts right
 	 * after it.
 	 */
-	if (mprotect(buf, pagesize, PROT_NONE)) {
+	if (mprotect(ret, pagesize, PROT_NONE)) {
 		perror("mprotect");
-		free(buf);
+		munmap(ret, pages * pagesize);
 		return NULL;
 	}
 
-	return buf + pagesize;
+	return ret + pagesize;
 }
 
 void tst_free_canary_left(void *ptr, size_t size __attribute__((unused)))
 {
 	size_t pagesize = sysconf(_SC_PAGESIZE);
+	size_t pages = size/pagesize + !!(size%pagesize) + 1;
 	void *start = ptr - pagesize;
 
-	/* Reset the memory protection back to RW */
-	if (mprotect(start, pagesize, PROT_READ | PROT_WRITE)) {
-		printf("%p size %zu\n", ptr, size);
-		perror("mprotect");
-	}
-
-	if (!real_free)
-		real_free = dlsym(RTLD_NEXT, "free");
-
-	real_free(start);
+	munmap(start, pages * pagesize);
 }
