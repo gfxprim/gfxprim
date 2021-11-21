gfxprim/gfxprim

commit 9890658b2a3cca7aea34525f6ae76d485263c75f

text: Rework text width functions
We have two different with funcitons now, one that returns bounding box
and second that returns advance up to a given point in string.

This also fixes the textbox selection "jumping" around since we have a
function to get the exact length to a start of the next chanracter now.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/text/gp_text_metric.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2009-2010 Jiri "BlueBear" Dluhos
  *                         <jiri.bluebear.dluhos@gmail.com>
  *
- * Copyright (C) 2009-2012 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2021 Cyril Hrubis <metan@ucw.cz>
  */
 
 #include "core/gp_common.h"
@@ -37,6 +37,16 @@ static unsigned int glyph_advance_x(const gp_text_style *style, int ch)
 }
 
 /*
+ * Returns glyph bearing.
+ */
+static unsigned int glyph_bearing_x(const gp_text_style *style, int ch)
+{
+	const gp_glyph *glyph = get_glyph(style, ch);
+
+	return multiply_width(style, glyph->bearing_x);
+}
+
+/*
  * Return maximal glyph advance for a character from str.
  */
 static unsigned int max_glyph_advance_x(const gp_text_style *style,
@@ -67,7 +77,14 @@ static unsigned int glyph_width(const gp_text_style *style, int c)
 }
 
 /*
- * Returns size occupied by the last glyph. Here we take advance_x into account.
+ * Returns size occupied by the last glyph.
+ *
+ * The end of the string is problematic too, some of the glyphs may have
+ * advance smaller than sum of the bitmap width and bearing_x that way we would
+ * return slightly less than is the actuall size of the text bouding box. The
+ * other problem is that the advance is usually grater than the glyph space (as
+ * the exact size of the glyph itself without advance before and space after is
+ * not known), so we likely return some more pixels than is needed.
  */
 static unsigned int last_glyph_width(const gp_text_style *style, int c)
 {
@@ -76,13 +93,17 @@ static unsigned int last_glyph_width(const gp_text_style *style, int c)
 	const gp_glyph *glyph = get_glyph(style, c);
 
 	advance = multiply_width(style, glyph->advance_x);
-	size    = multiply_width(style, glyph->width + glyph->bearing_x);
+	size = multiply_width(style, glyph->width + glyph->bearing_x);
 
 	return (size > advance) ? size : advance;
 }
 
 /*
  * Returns first glyph width, that is and advance minus the bearing_x.
+ *
+ * First letter may have bearing_x negative, making it overflow out of the
+ * bouding box and even in case it's possitive the returned size would be
+ * slightly bigger.
  */
 static unsigned int first_glyph_width(const gp_text_style *style, int c)
 {
@@ -101,37 +122,17 @@ static const gp_text_style *assert_style(const gp_text_style *style)
 	return style;
 }
 
-/*
- * Returns text width.
- *
- * There are two problems with text width it's start and it's end.
- *
- * First letter may have bearing_x negative, making it overflow out of the
- * bouding box and even in case it's possitive the returned size would be
- * slightly bigger. This one is easy to fix.
- *
- * The end of the string is problematic too, some of the glyphs may have
- * advance smaller than sum of the bitmap width and bearing_x that way we would
- * return slightly less than is the actuall size of the text bouding box. The
- * other problem is that the advance is usually grater than the glyph space (as
- * the exact size of the glyph itself without advance before and space after is
- * not known), so we likely return some more pixels than is needed, but that's
- * the joy of life.
- */
-unsigned int gp_text_width_len(const gp_text_style *style,
-                               const char *str, size_t len)
+gp_size gp_text_width_len(const gp_text_style *style, enum gp_text_len_type type,
+                          const char *str, size_t len)
 {
 	unsigned int i, ret;
 
 	style = assert_style(style);
 
-	if (str == NULL || str[0] == '\0')
+	if (!str || !str[0] || !len)
 		return 0;
 
-	if (len == 0)
-		return 0;
-
-	/* special case, one letter */
+	/* special case, single letter */
 	if (str[1] == '\0' || len == 1)
 		return glyph_width(style, str[0]);
 
@@ -139,18 +140,31 @@ unsigned int gp_text_width_len(const gp_text_style *style,
 	ret = first_glyph_width(style, str[0]) + style->char_xspace;
 
 	/* middle letters */
-	for (i = 1; i + 1 < len && str[i+1]; i++)
-		ret += glyph_advance_x(style, str[i]) + style->char_xspace;
+	for (i = 1; i + 1 < len && str[i+1]; i++) {
+		ret += glyph_bearing_x(style, str[i]);
+		ret += glyph_advance_x(style, str[i]);
+		ret += style->char_xspace;
+	}
 
 	/* last letter */
-	ret += last_glyph_width(style, str[i]);
+	switch (type) {
+	case GP_TEXT_LEN_BBOX:
+		ret += last_glyph_width(style, str[i]);
+	break;
+	case GP_TEXT_LEN_ADVANCE:
+		ret += glyph_advance_x(style, str[i]) + style->char_xspace;
+
+		if (str[i+1])
+			ret += glyph_bearing_x(style, str[i+1]);
+	break;
+	}
 
 	return ret;
 }
 
-unsigned int gp_text_width(const gp_text_style *style, const char *str)
+gp_size gp_text_width(const gp_text_style *style, enum gp_text_len_type type,const char *str)
 {
-	return gp_text_width_len(style, str, SIZE_MAX);
+	return gp_text_width_len(style, type, str, SIZE_MAX);
 }
 
 gp_size gp_text_max_width(const gp_text_style *style, unsigned int len)

demos/c_simple/fonttest.c
@@ -109,7 +109,7 @@ void redraw_screen(void)
 
 		gp_fill_rect_xywh(win->pixmap,
 			16, SPACING*i + 16,
-			gp_text_width(&style, test_string),
+			gp_text_wbbox(&style, test_string),
 			gp_font_height(style.font),
 			dark_gray_pixel);
 
@@ -136,7 +136,7 @@ void redraw_screen(void)
 		        white_pixel, black_pixel, test_string);
 
 		gp_rect_xywh(win->pixmap, 33, SPACING * i + 43,
-		             gp_text_width(&style, test_string) + 1,
+		             gp_text_wbbox(&style, test_string) + 1,
 			     gp_text_height(&style) + 1, dark_gray_pixel);
 
 		style.pixel_xmul = 4;

demos/spiv/spiv.c
@@ -79,7 +79,7 @@ static int image_loader_callback(gp_progress_cb *self)
 
 	int align = GP_ALIGN_CENTER|GP_VALIGN_ABOVE;
 
-	size = gp_text_width(config.style, buf);
+	size = gp_text_wbbox(config.style, buf);
 
 	int start = c->w/2 - size/2 - 10;
 	int end   = c->w/2 + size/2 + 10;

demos/spiv/spiv_help.c
@@ -233,7 +233,7 @@ static int redraw_help(gp_backend *backend, unsigned int loff, gp_coord xoff)
 	unsigned int max = 0;
 
 	for (i = 0; i < help_keys_len; i++)
-		max = GP_MAX(max, gp_text_width(config.style, help_keys[i].keys));
+		max = GP_MAX(max, gp_text_wbbox(config.style, help_keys[i].keys));
 
 	for (i = loff; i < help_keys_len; i++) {
 		gp_coord h = spacing + (i - loff + 1) * (height + spacing);

include/text/gp_text_metric.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2009-2010 Jiri "BlueBear" Dluhos
  *                         <jiri.bluebear.dluhos@gmail.com>
  *
- * Copyright (C) 2009-2011 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2021 Cyril Hrubis <metan@ucw.cz>
  */
 
 #ifndef TEXT_GP_TEXT_METRIC_H
@@ -12,13 +12,71 @@
 #include <core/gp_types.h>
 #include <text/gp_text_style.h>
 
-/*
- * Calculates the width of the string drawn in the given style, in pixels.
+enum gp_text_len_type {
+	/*
+	 * Return bounding box, i.e. for the last letter return the greater of
+	 * advance and letter width. This makes sure that the resulting size
+	 * will fit the text.
+	 */
+	GP_TEXT_LEN_BBOX,
+	/*
+	 * Returns advance, i.e. where next letter would start after a string
+	 * would have been printed. This may return slightly less than the
+	 * GP_TEXT_LEN_BBOX.
+	 */
+	GP_TEXT_LEN_ADVANCE,
+};
+
+/**
+ * @brief Calculates the width of the string drawn in the given style.
+ *
+ * @style A text style + font formatting.
+ * @type Select if we want a bounding box or advance.
+ * @str A string.
+ * @len Maximal lenght of the string.
+ *
+ * @return Width in pixels.
  */
-gp_size gp_text_width_len(const gp_text_style *style,
+gp_size gp_text_width_len(const gp_text_style *style, enum gp_text_len_type type,
                           const char *str, size_t len);
 
-gp_size gp_text_width(const gp_text_style *style, const char *str);
+/**
+ * @brief Calculates bounding box width of the string drawn in the given style.
+ *
+ * @style A text style + font formatting.
+ * @str A string.
+ * @len Maximal lenght of the string.
+ *
+ * @return Width in pixels.
+ */
+static inline gp_size gp_text_wbbox_len(const gp_text_style *style, const char *str, size_t len)
+{
+	return gp_text_width_len(style, GP_TEXT_LEN_BBOX, str, len);
+}
+
+/**
+ * @brief Calculates the width of the string drawn in the given style.
+ *
+ * @style A text style + font formatting.
+ * @type Select if we want a bounding box or advance.
+ * @str A string.
+ *
+ * @return Width in pixels.
+ */
+gp_size gp_text_width(const gp_text_style *style, enum gp_text_len_type type, const char *str);
+
+/**
+ * @brief Calculates bounding box width of the string drawn in the given style.
+ *
+ * @style A text style + font formatting.
+ * @str A string.
+ *
+ * @return Width in pixels.
+ */
+static inline gp_size gp_text_wbbox(const gp_text_style *style, const char *str)
+{
+	return gp_text_width(style, GP_TEXT_LEN_BBOX, str);
+}
 
 /*
  * Maximal text width for string with len characters.

libs/text/gp_text.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2009-2011 Jiri "BlueBear" Dluhos
  *                         <jiri.bluebear.dluhos@gmail.com>
  *
- * Copyright (C) 2009-2014 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2021 Cyril Hrubis <metan@ucw.cz>
  */
 
 #include <core/gp_pixmap.h>
@@ -82,7 +82,7 @@ gp_size gp_text_ext(gp_pixmap *pixmap, const gp_text_style *style,
 
 	gp_coord topleft_x, topleft_y;
 
-	gp_size w = gp_text_width(style, str);
+	gp_size w = gp_text_width(style, GP_TEXT_LEN_BBOX, str);
 
 	GP_ASSERT(do_align(&topleft_x, &topleft_y, align, x, y, style, w) == 0,
 	         "Invalid aligment flags");
@@ -141,5 +141,5 @@ void gp_text_clear_str(gp_pixmap *pixmap, const gp_text_style *style,
 		       gp_pixel bg_color, const char *str)
 {
 	gp_text_clear(pixmap, style, x, y, align,
-	              bg_color, gp_text_width(style, str));
+	              bg_color, gp_text_width(style, GP_TEXT_LEN_BBOX, str));
 }

libs/text/gp_text.gen.c.t
@@ -235,5 +235,5 @@ gp_size gp_text_raw(gp_pixmap *pixmap, const gp_text_style *style,
 		GP_ABORT("Invalid font glyph bitmap format");
 	}
 
-	return gp_text_width_len(style, str, max_chars);
+	return gp_text_width_len(style, GP_TEXT_LEN_BBOX, str, max_chars);
 }

libs/widgets/gp_widget_button.c
@@ -18,13 +18,13 @@ static unsigned int min_w(gp_widget *self, const gp_widget_render_ctx *ctx)
 	unsigned int ret = 2 * ctx->padd;
 
 	if (self->b->label)
-		ret += gp_text_width(ctx->font, self->b->label);
+		ret += gp_text_wbbox(ctx->font, self->b->label);
 
 	if (self->b->type & GP_BUTTON_TYPE_MASK)
 		ret += GP_ODD_UP(gp_text_ascent(ctx->font));
 
 	if (self->b->label && self->b->type & GP_BUTTON_TYPE_MASK)
-		ret += gp_text_width(ctx->font, " ");
+		ret += gp_text_wbbox(ctx->font, " ");
 
 	return ret;
 }
@@ -110,7 +110,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 	if (self->b->label) {
 		unsigned int tcx = cx, len;
-		unsigned int spc = gp_text_width(ctx->font, " ");
+		unsigned int spc = gp_text_wbbox(ctx->font, " ");
 
 		if (self->b->type & GP_BUTTON_TYPE_MASK) {
 			if (self->b->type & GP_BUTTON_TEXT_LEFT)

commit a594711548deb4bd4d0bbbbdb6b9ae1d4121207d

text metric: We must not add bearing in the middle of string
Fixes: 9890658b2a3c (text: Rework text width functions)
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/text/gp_text_metric.c
@@ -141,7 +141,6 @@ gp_size gp_text_width_len(const gp_text_style *style, enum gp_text_len_type type
 
 	/* middle letters */
 	for (i = 1; i + 1 < len && str[i+1]; i++) {
-		ret += glyph_bearing_x(style, str[i]);
 		ret += glyph_advance_x(style, str[i]);
 		ret += style->char_xspace;
 	}