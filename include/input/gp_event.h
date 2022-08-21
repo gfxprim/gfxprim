gfxprim/gfxprim

commit ca01cf7b894895a1415a132fb15d7dfc44d424f7

input/gp_event.h: Remove forgotten function declaration
The function is now in input/gp_keys.h

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

include/input/gp_event.h
@@ -175,11 +175,6 @@ int gp_event_all_keys_pressed_(gp_event *ev, ...);
 void gp_event_dump(gp_event *ev);
 
 /*
- * Returns human-readable key name.
- */
-const char *gp_event_key_name(enum gp_event_key_value key);
-
-/*
  * Retruns difference between two timeval structures in miliseconds
  */
 static inline int64_t gp_timeval_diff_ms(struct timeval a, struct timeval b)

commit c55b76b2d1e760637361bd224da72ba750b4f1f0

input: event.h: include gp_keys.h
We have to include the header so that the function is defined

Fixes: ca01cf7b894895 (input/gp_event.h: Remove forgotten function declaration)
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

include/input/gp_event.h
@@ -21,6 +21,7 @@
 #include <sys/time.h>
 
 #include <input/gp_types.h>
+#include <input/gp_keys.h>
 #include <input/gp_event_keys.h>
 
 enum gp_event_type {