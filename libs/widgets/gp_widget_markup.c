gfxprim/gfxprim

commit 0d428bf39dfa950507d09f8ff386b642b4f1e60b

widgets: Add disabled attribute
When widget is disabled it does not process events and is "grayed out".
All child widgets of disabled widget are disabled as well.

TODO:

- Stock is not grayed out properly
- There is no proper API to disable widget and trigger proper redraw yet

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_markup.c
@@ -108,6 +108,10 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int w = self->w;
 	unsigned int h = self->h;
+	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	gp_widget_ops_blit(ctx, x, y, w, h);
 	gp_fill_rect_xywh(ctx->buf, x, y, w, h, ctx->bg_color);
@@ -128,7 +132,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 			if (e->type == GP_MARKUP_END)
 				return;
 
-			gp_pixel fg = ctx->text_color;
+			gp_pixel fg = text_color;
 			gp_pixel bg = ctx->bg_color;
 
 			if (e->attrs & GP_MARKUP_SUBSCRIPT)

include/widgets/gp_widget.h
@@ -78,6 +78,11 @@ struct gp_widget {
 	 */
 	unsigned int no_events:1;
 
+	/*
+	 * If set widget is 'grayed out' and no events are processed.
+	 */
+	unsigned int disabled:1;
+
 	uint32_t event_mask;
 
 	union {

include/widgets/gp_widget_ops.h
@@ -40,6 +40,8 @@ enum gp_widget_render_flags {
 	GP_WIDGET_COLOR_SCHEME = 0x04,
 	/** Layout needs to be resized */
 	GP_WIDGET_RESIZE = 0x08,
+	/** Widget is disabled */
+	GP_WIDGET_DISABLED = 0x10,
 };
 
 struct gp_widget_ops {
@@ -254,6 +256,11 @@ static inline void gp_widget_ops_blit(const gp_widget_render_ctx *ctx,
 static inline int gp_widget_should_redraw(gp_widget *self, int flags)
 {
 	return self->redraw || (flags & GP_WIDGET_REDRAW);
+}
+
+static inline int gp_widget_is_disabled(gp_widget *self, int flags)
+{
+	return self->disabled || (flags & GP_WIDGET_DISABLED);
 }
 
 /**

include/widgets/gp_widget_render.h
@@ -37,6 +37,7 @@ struct gp_widget_render_ctx {
 			gp_pixel warn_color;
 			gp_pixel accept_color;
 			gp_pixel fill_color;
+			gp_pixel col_disabled;
 		};
 	};
 

include/widgets/gp_widgets_color_scheme.h
@@ -33,6 +33,7 @@ enum gp_widgets_color {
 	GP_WIDGETS_COL_WARN,
 	GP_WIDGETS_COL_ACCEPT,
 	GP_WIDGETS_COL_FILL,
+	GP_WIDGETS_COL_DISABLED,
 	GP_WIDGETS_THEME_COLORS,
 
 	/* 16 colors */

libs/widgets/gp_widget_button.c
@@ -91,13 +91,15 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int w = self->w;
 	unsigned int h = self->h;
+	gp_pixel text_color = ctx->text_color;
 
-	(void)flags;
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	gp_widget_ops_blit(ctx, x, y, w, h);
 
 	gp_pixel bg_color = self->b->val ? ctx->bg_color : ctx->fg_color;
-	gp_pixel fr_color = self->focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel fr_color = self->focused ? ctx->sel_color : text_color;
 
 	gp_fill_rrect_xywh(ctx->buf, x, y, w, h, ctx->bg_color, bg_color, fr_color);
 
@@ -121,7 +123,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 		len = gp_text(ctx->buf, ctx->font,
 			tcx, cy-(asc-asc_half), GP_ALIGN_CENTER|GP_VALIGN_BELOW,
-			ctx->text_color, bg_color, self->b->label);
+			text_color, bg_color, self->b->label);
 
 		if (self->b->type & GP_BUTTON_TEXT_LEFT)
 			cx += len/2 + spc/2;

libs/widgets/gp_widget_checkbox.c
@@ -45,8 +45,10 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int w = self->w;
 	unsigned int h = self->h;
+	gp_pixel text_color = ctx->text_color;
 
-	(void)flags;
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	gp_widget_ops_blit(ctx, x, y, w, h);
 
@@ -54,7 +56,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 	y += ctx->padd;
 
-	gp_pixel color = self->focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel color = self->focused ? ctx->sel_color : text_color;
 
 	gp_fill_rrect_xywh(ctx->buf, x, y, text_a, text_a, ctx->bg_color, ctx->fg_color, color);
 
@@ -69,8 +71,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	gp_text(ctx->buf, ctx->font,
 		x + text_a + ctx->padd, y,
 		GP_ALIGN_RIGHT|GP_VALIGN_BELOW,
-		ctx->text_color,
-		ctx->bg_color, self->b->label);
+		text_color, ctx->bg_color, self->b->label);
 }
 
 static void set(gp_widget *self, int val)

libs/widgets/gp_widget_frame.c
@@ -80,6 +80,10 @@ static void render(gp_widget *self, const gp_offset *offset,
 	struct gp_widget_frame *frame = self->frame;
 	struct gp_widget *payload = frame->child;
 	const gp_text_style *font = gp_widget_tattr_font(self->frame->tattr, ctx);
+	gp_pixel text_color = ctx->text_color;
+
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	if (gp_widget_should_redraw(self, flags)) {
 		gp_widget_ops_blit(ctx, x, y, w, h);
@@ -89,7 +93,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 		gp_fill_rect_xywh(ctx->buf, x, y, w, gp_text_height(font)/2, ctx->bg_color);
 
 		gp_fill_rrect_xywh(ctx->buf, x, y + payload_off_y(self, ctx)/2, w,
-		              h - payload_off_y(self, ctx)/2, ctx->bg_color, fg_color, ctx->text_color);
+		              h - payload_off_y(self, ctx)/2, ctx->bg_color, fg_color, text_color);
 
 		if (frame->title) {
 			unsigned int sw = gp_text_wbbox(font, self->frame->title) + ctx->padd;
@@ -97,7 +101,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 			gp_hline_xyw(ctx->buf, x + ctx->padd + ctx->padd/2, y + payload_off_y(self, ctx)/2, sw, fg_color);
 
 			gp_text(ctx->buf, font, x + 2 * ctx->padd, y, GP_ALIGN_RIGHT|GP_VALIGN_BELOW,
-				ctx->text_color, ctx->bg_color, self->frame->title);
+				text_color, ctx->bg_color, self->frame->title);
 		}
 	}
 

commit c20e63dc603afe5d3bd7b79b0d0bbd2b739f46e5

widgets: markump: Fix copy&paste error
Fixes: 0d428bf39dfa (widgets: Add disabled attribute)
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_markup.c
@@ -108,7 +108,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int w = self->w;
 	unsigned int h = self->h;
-	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+	gp_pixel text_color = ctx->text_color;
 
 	if (gp_widget_is_disabled(self, flags))
 		text_color = ctx->col_disabled;