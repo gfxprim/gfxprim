gfxprim/gfxprim

commit 0d428bf39dfa950507d09f8ff386b642b4f1e60b

widgets: Add disabled attribute
When widget is disabled it does not process events and is "grayed out".
All child widgets of disabled widget are disabled as well.

TODO:

- Stock is not grayed out properly
- There is no proper API to disable widget and trigger proper redraw yet

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_scroll_area.c
@@ -137,7 +137,7 @@ static gp_coord max_y_off(gp_widget *self)
 }
 
 static void draw_vert_scroll_bar(gp_widget *self, const gp_widget_render_ctx *ctx,
-                                 gp_coord x, gp_coord y, gp_size h, gp_size size)
+                                 gp_coord x, gp_coord y, gp_size h, gp_size size, gp_pixel color)
 {
 	struct gp_widget_scroll_area *area = self->scroll;
 	gp_size asc = gp_text_ascent(ctx->font);
@@ -146,18 +146,18 @@ static void draw_vert_scroll_bar(gp_widget *self, const gp_widget_render_ctx *ct
 
 	gp_size sh = scrollbar_h(self, ctx);
 
-	gp_vline_xyh(ctx->buf, x + ctx->padd + asc/2, y, sh, ctx->text_color);
+	gp_vline_xyh(ctx->buf, x + ctx->padd + asc/2, y, sh, color);
 
 	gp_size max_off = max_y_off(self);
 	gp_coord pos = ((sh - asc) * area->y_off + max_off/2) / max_off;
 
-	gp_pixel col = area->area_focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel col = area->area_focused ? ctx->sel_color : color;
 
 	gp_fill_rrect_xywh(ctx->buf, x + ctx->padd, y + pos, asc, asc, ctx->bg_color, ctx->fg_color, col);
 }
 
 static void draw_horiz_scroll_bar(gp_widget *self, const gp_widget_render_ctx *ctx,
-                                  gp_coord x, gp_coord y, gp_size w, gp_size size)
+                                  gp_coord x, gp_coord y, gp_size w, gp_size size, gp_pixel color)
 {
 	struct gp_widget_scroll_area *area = self->scroll;
 	gp_size asc = gp_text_ascent(ctx->font);
@@ -166,12 +166,12 @@ static void draw_horiz_scroll_bar(gp_widget *self, const gp_widget_render_ctx *c
 
 	gp_size sw = scrollbar_w(self, ctx);
 
-	gp_hline_xyw(ctx->buf, x, y + ctx->padd + asc/2, sw, ctx->text_color);
+	gp_hline_xyw(ctx->buf, x, y + ctx->padd + asc/2, sw, color);
 
 	gp_size max_off = max_x_off(self);
 	gp_coord pos = ((sw - asc) * area->x_off + max_off/2) / max_off;
 
-	gp_pixel col = area->area_focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel col = area->area_focused ? ctx->sel_color : color;
 
 	gp_fill_rrect_xywh(ctx->buf, x + pos, y + ctx->padd, asc, asc, ctx->bg_color, ctx->fg_color, col);
 }
@@ -186,6 +186,10 @@ static void render(gp_widget *self, const gp_offset *offset,
 		.x = -area->x_off,
 		.y = -area->y_off,
 	};
+	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	//TODO!!!
 	gp_widget_ops_blit(ctx, self->x + offset->x, self->y + offset->y, self->w, self->h);
@@ -196,12 +200,12 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 	if (area->scrollbar_x) {
 		w -= size;
-		draw_vert_scroll_bar(self, ctx, self->x + offset->x + w, self->y + offset->y, h, size);
+		draw_vert_scroll_bar(self, ctx, self->x + offset->x + w, self->y + offset->y, h, size, text_color);
 	}
 
 	if (area->scrollbar_y) {
 		h -= size;
-		draw_horiz_scroll_bar(self, ctx, self->x + offset->x, self->y + offset->y + h, w, size);
+		draw_horiz_scroll_bar(self, ctx, self->x + offset->x, self->y + offset->y + h, w, size, text_color);
 	}
 
 	gp_sub_pixmap(ctx->buf, &child_buf,
@@ -218,7 +222,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	child_ctx.flip = NULL;
 
 	gp_widget_ops_render(area->child, &child_offset, &child_ctx, flags);
-	gp_rect_xywh(ctx->buf, self->x + offset->x, self->y + offset->y, w, h, ctx->text_color);
+	gp_rect_xywh(ctx->buf, self->x + offset->x, self->y + offset->y, w, h, text_color);
 }
 
 static int is_in_scrollbar_x(gp_widget *self, const gp_widget_render_ctx *ctx, unsigned int x)

libs/widgets/gp_widget_spin_button.c
@@ -48,19 +48,21 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int s = buttons_width(ctx);
 	unsigned int sy = (gp_text_ascent(ctx->font)/2 + ctx->padd)/5;
 	unsigned int sx = 2*sy;
+	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
 
-	(void)flags;
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	gp_widget_ops_blit(ctx, x, y, w, h);
 
-	gp_pixel color = self->focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel color = self->focused ? ctx->sel_color : text_color;
 
 	gp_fill_rrect_xywh(ctx->buf, x, y, w, h,
 	                   ctx->bg_color, ctx->fg_color, color);
 
 	gp_print(ctx->buf, ctx->font, x + ctx->padd, y + ctx->padd,
 		 GP_ALIGN_RIGHT | GP_VALIGN_BELOW,
-		 ctx->text_color, ctx->bg_color, "%s",
+		 text_color, ctx->bg_color, "%s",
 		 self->choice->ops->get_choice(self, self->choice->sel));
 
 	gp_coord rx = x + w - s;
@@ -71,14 +73,14 @@ static void render(gp_widget *self, const gp_offset *offset,
 	if (self->choice->sel == 0)
 		color = ctx->bg_color;
 	else
-		color = ctx->text_color;
+		color = text_color;
 
 	gp_symbol(ctx->buf, x + w - s/2 - 1, y + h/4, sx, sy, GP_TRIANGLE_UP, color);
 
 	if (self->choice->sel + 1 >= gp_widget_choice_cnt_get(self))
 		color = ctx->bg_color;
 	else
-		color = ctx->text_color;
+		color = text_color;
 
 	gp_symbol(ctx->buf, x + w - s/2 - 1, y + (3*h)/4, sx, sy, GP_TRIANGLE_DOWN, color);
 }

libs/widgets/gp_widget_tabs.c
@@ -113,6 +113,10 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int tab_h = title_h(self, ctx);
 	unsigned int act_x = 0, act_w = 0;
+	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	if (self->redraw)
 		gp_widget_ops_blit(ctx, x, y, self->w, self->h);
@@ -156,20 +160,20 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 		gp_text(ctx->buf, font, cur_x + w/2, y + ctx->padd,
 			GP_ALIGN_CENTER|GP_VALIGN_BELOW,
-			ctx->text_color, ctx->bg_color, label);
+			text_color, ctx->bg_color, label);
 
 		cur_x += w;
 
 		if (cur_x < x + self->w)
-			gp_vline_xyh(ctx->buf, cur_x-1, y+1, tab_h-1, ctx->text_color);
+			gp_vline_xyh(ctx->buf, cur_x-1, y+1, tab_h-1, text_color);
 	}
 
 	if (!active_first(self))
-		gp_hline_xxy(ctx->buf, x, act_x-1, y + tab_h, ctx->text_color);
+		gp_hline_xxy(ctx->buf, x, act_x-1, y + tab_h, text_color);
 
-	gp_hline_xxy(ctx->buf, act_x + act_w - 1, x + self->w-1, y + tab_h, ctx->text_color);
+	gp_hline_xxy(ctx->buf, act_x + act_w - 1, x + self->w-1, y + tab_h, text_color);
 
-	gp_rrect_xywh(ctx->buf, x, y, self->w, self->h, ctx->text_color);
+	gp_rrect_xywh(ctx->buf, x, y, self->w, self->h, text_color);
 
 	if (!widget)
 		return;

libs/widgets/gp_widget_tbox.c
@@ -76,11 +76,14 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int w = self->w;
 	unsigned int h = self->h;
 	const char *str = tbox_visible_str(tbox);
-	(void)flags;
+	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+
+	if (gp_widget_is_disabled(self, flags))
+		text_color = ctx->col_disabled;
 
 	gp_widget_ops_blit(ctx, x, y, w, h);
 
-	gp_pixel color = self->focused ? ctx->sel_color : ctx->text_color;
+	gp_pixel color = self->focused ? ctx->sel_color : text_color;
 
 	if (tbox->alert) {
 		color = ctx->alert_color;
@@ -116,15 +119,15 @@ static void render(gp_widget *self, const gp_offset *offset,
 	if (left.bytes) {
 		gp_coord cx = x + ctx->padd/2;
 
-		gp_line(ctx->buf, cx-s, cy, cx, cy-s, ctx->text_color);
-		gp_line(ctx->buf, cx-s, cy, cx, cy+s, ctx->text_color);
+		gp_line(ctx->buf, cx-s, cy, cx, cy-s, text_color);
+		gp_line(ctx->buf, cx-s, cy, cx, cy+s, text_color);
 	}
 
 	if (right.bytes < gp_vec_strlen(tbox->buf)) {
 		gp_coord cx = x + w - 1 - ctx->padd/2;
 
-		gp_line(ctx->buf, cx+s, cy, cx, cy-s, ctx->text_color);
-		gp_line(ctx->buf, cx+s, cy, cx, cy+s, ctx->text_color);
+		gp_line(ctx->buf, cx+s, cy, cx, cy-s, text_color);
+		gp_line(ctx->buf, cx+s, cy, cx, cy+s, text_color);
 	}
 
 	if (self->focused && !is_sel(self)) {
@@ -133,14 +136,14 @@ static void render(gp_widget *self, const gp_offset *offset,
 		                              tbox->cur_pos.chars - left.chars);
 
 		gp_vline_xyh(ctx->buf, cursor_x, y + ctx->padd,
-			     gp_text_ascent(font), ctx->text_color);
+			     gp_text_ascent(font), text_color);
 	}
 
 	str += left.bytes;
 	gp_text_ext(ctx->buf, font,
 		    x + ctx->padd, y + ctx->padd,
 		    GP_ALIGN_RIGHT|GP_VALIGN_BELOW,
-		    ctx->text_color, ctx->bg_color, str, right.chars - left.chars);
+		    text_color, ctx->bg_color, str, right.chars - left.chars);
 
 	if (!is_sel(self))
 		return;
@@ -163,7 +166,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 
 	gp_text_ext(ctx->buf, font, sel_x_off, y + ctx->padd,
 	            GP_ALIGN_RIGHT | GP_VALIGN_BELOW,
-	            ctx->text_color, ctx->sel_color, str,
+	            text_color, ctx->sel_color, str,
 		    sel_len);
 }
 

include/widgets/gp_widget.h
@@ -78,6 +78,11 @@ struct gp_widget {
 	 */
 	unsigned int no_events:1;
 
+	/*
+	 * If set widget is 'grayed out' and no events are processed.
+	 */
+	unsigned int disabled:1;
+
 	uint32_t event_mask;
 
 	union {

include/widgets/gp_widget_ops.h
@@ -40,6 +40,8 @@ enum gp_widget_render_flags {
 	GP_WIDGET_COLOR_SCHEME = 0x04,
 	/** Layout needs to be resized */
 	GP_WIDGET_RESIZE = 0x08,
+	/** Widget is disabled */
+	GP_WIDGET_DISABLED = 0x10,
 };
 
 struct gp_widget_ops {
@@ -254,6 +256,11 @@ static inline void gp_widget_ops_blit(const gp_widget_render_ctx *ctx,
 static inline int gp_widget_should_redraw(gp_widget *self, int flags)
 {
 	return self->redraw || (flags & GP_WIDGET_REDRAW);
+}
+
+static inline int gp_widget_is_disabled(gp_widget *self, int flags)
+{
+	return self->disabled || (flags & GP_WIDGET_DISABLED);
 }
 
 /**

include/widgets/gp_widget_render.h
@@ -37,6 +37,7 @@ struct gp_widget_render_ctx {
 			gp_pixel warn_color;
 			gp_pixel accept_color;
 			gp_pixel fill_color;
+			gp_pixel col_disabled;
 		};
 	};
 

include/widgets/gp_widgets_color_scheme.h
@@ -33,6 +33,7 @@ enum gp_widgets_color {
 	GP_WIDGETS_COL_WARN,
 	GP_WIDGETS_COL_ACCEPT,
 	GP_WIDGETS_COL_FILL,
+	GP_WIDGETS_COL_DISABLED,
 	GP_WIDGETS_THEME_COLORS,
 
 	/* 16 colors */

commit 86f067d22aad8b8ce9aea673503e670bb34157af

widgets: Fix invalid reads
Fixes: 0d428bf39dfa (widgets: Add disabled attribute)
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_scroll_area.c
@@ -186,7 +186,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 		.x = -area->x_off,
 		.y = -area->y_off,
 	};
-	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+	gp_pixel text_color = ctx->text_color;
 
 	if (gp_widget_is_disabled(self, flags))
 		text_color = ctx->col_disabled;

libs/widgets/gp_widget_spin_button.c
@@ -48,7 +48,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int s = buttons_width(ctx);
 	unsigned int sy = (gp_text_ascent(ctx->font)/2 + ctx->padd)/5;
 	unsigned int sx = 2*sy;
-	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+	gp_pixel text_color = ctx->text_color;
 
 	size_t sel = call_get_sel(self);
 	size_t cnt = call_get_cnt(self);

libs/widgets/gp_widget_tabs.c
@@ -128,7 +128,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int tab_h = title_h(self, ctx);
 	unsigned int act_x = 0, act_w = 0;
-	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+	gp_pixel text_color = ctx->text_color;
 
 	if (gp_widget_is_disabled(self, flags))
 		text_color = ctx->col_disabled;

libs/widgets/gp_widget_tbox.c
@@ -75,7 +75,7 @@ static void render(gp_widget *self, const gp_offset *offset,
 	unsigned int y = self->y + offset->y;
 	unsigned int w = self->w;
 	unsigned int h = self->h;
-	gp_pixel text_color = gp_widgets_color(ctx, self->label->text_color);
+	gp_pixel text_color = ctx->text_color;
 
 	if (gp_widget_is_disabled(self, flags))
 		text_color = ctx->col_disabled;