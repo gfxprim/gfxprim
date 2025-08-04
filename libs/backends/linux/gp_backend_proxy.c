gfxprim/gfxprim

commit 2c7aad7f709cc275e1aa99fe853815ee63fe2a27

backends: proxy: Synchronize backend updates.
Make the server send an event back once it copied the data from the
shared memory. This fixes cases where application rewrote the proxy
buffer while server was copying the data.

Now the client waits until it receives event that the update was
finished.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/linux/gp_backend_proxy.c
@@ -32,6 +32,8 @@ struct proxy_priv {
 	unsigned int visible:1;
 	unsigned int sys_quit_requested:1;
 
+	unsigned int update_in_progress:1;
+
 	/* mapped memory backing the pixmap */
 	void *map;
 	size_t map_size;
@@ -146,60 +148,23 @@ static void init_pixmap(gp_backend *self, union gp_proxy_msg *msg)
 	gp_ev_queue_set_screen_size(self->event_queue, msg->pix.pix.w, msg->pix.pix.h);
 }
 
-static enum gp_poll_event_ret proxy_process_fd(gp_fd *self)
+/**
+ * @brief Receives data and parses them into event(s).
+ *
+ * Reads data from the client socket and parses them into event(s).
+ *
+ * @param self A fd.
+ * @block If set the call blocks until data are received.
+ * @return A number of bytes read or -1 on a failure.
+ */
+static int proxy_recv_events(gp_fd *self, int block)
 {
 	gp_backend *backend = self->priv;
 	struct proxy_priv *priv = GP_BACKEND_PRIV(backend);
 	union gp_proxy_msg *msg;
 	int ret;
 
-	while ((ret = gp_proxy_buf_recv(priv->fd.fd, &priv->buf)) > 0) {
-		while (gp_proxy_next(&priv->buf, &msg)) {
-			switch (msg->type) {
-			case GP_PROXY_CLI_INIT:
-				GP_DEBUG(4, "Got GP_PROXY_CLI_INIT");
-				priv->dummy.pixel_type = msg->cli_init.cli_init.pixel_type;
-				backend->dpi = msg->cli_init.cli_init.dpi;
-			break;
-			case GP_PROXY_EVENT:
-				GP_DEBUG(4, "Got GP_PROXY_EVENT");
-				gp_ev_queue_put(backend->event_queue, &msg->ev.ev);
-			break;
-			case GP_PROXY_MAP:
-				GP_DEBUG(4, "Got GP_PROXY_MAP");
-				map_buffer(backend, msg);
-			break;
-			case GP_PROXY_UNMAP:
-				GP_DEBUG(4, "Got GP_PROXY_UNMAP");
-				unmap_buffer(backend);
-			break;
-			case GP_PROXY_PIXMAP:
-				GP_DEBUG(4, "Got GP_PROXY_PIXMAP");
-				init_pixmap(backend, msg);
-			break;
-			case GP_PROXY_SHOW:
-				GP_DEBUG(4, "Got GP_PROXY_SHOW");
-				visible(backend);
-			break;
-			case GP_PROXY_HIDE:
-				GP_DEBUG(4, "Got GP_PROXY_HIDE");
-				hidden(backend);
-			break;
-			case GP_PROXY_CURSOR_POS:
-				GP_DEBUG(4, "Got GP_PROXY_CURSOR_POS");
-				gp_ev_queue_set_cursor_pos(backend->event_queue,
-				                           msg->cursor.pos.x,
-				                           msg->cursor.pos.y);
-			break;
-			case GP_PROXY_EXIT:
-				GP_DEBUG(4, "Got GP_PROXY_EXIT");
-				gp_ev_queue_push(backend->event_queue, GP_EV_SYS,
-				                 GP_EV_SYS_QUIT, 0, 0);
-				priv->sys_quit_requested = 1;
-			break;
-			}
-		}
-	}
+	ret = gp_proxy_buf_recv(priv->fd.fd, &priv->buf, block);
 
 	if (ret == 0) {
 		if (priv->sys_quit_requested) {
@@ -210,7 +175,71 @@ static enum gp_poll_event_ret proxy_process_fd(gp_fd *self)
 		GP_WARN("Connection closed");
 		gp_ev_queue_push(backend->event_queue, GP_EV_SYS, GP_EV_SYS_QUIT, 0, 0);
 		priv->sys_quit_requested = 1;
+
+		return 0;
 	}
+
+	if (ret < 0)
+		return ret;
+
+	while (gp_proxy_next(&priv->buf, &msg)) {
+		switch (msg->type) {
+		case GP_PROXY_CLI_INIT:
+			GP_DEBUG(4, "Got GP_PROXY_CLI_INIT");
+			priv->dummy.pixel_type = msg->cli_init.cli_init.pixel_type;
+			backend->dpi = msg->cli_init.cli_init.dpi;
+		break;
+		case GP_PROXY_EVENT:
+			GP_DEBUG(4, "Got GP_PROXY_EVENT");
+			gp_ev_queue_put(backend->event_queue, &msg->ev.ev);
+		break;
+		case GP_PROXY_MAP:
+			GP_DEBUG(4, "Got GP_PROXY_MAP");
+			map_buffer(backend, msg);
+		break;
+		case GP_PROXY_UNMAP:
+			GP_DEBUG(4, "Got GP_PROXY_UNMAP");
+			unmap_buffer(backend);
+		break;
+		case GP_PROXY_PIXMAP:
+			GP_DEBUG(4, "Got GP_PROXY_PIXMAP");
+			init_pixmap(backend, msg);
+		break;
+		case GP_PROXY_SHOW:
+			GP_DEBUG(4, "Got GP_PROXY_SHOW");
+			visible(backend);
+		break;
+		case GP_PROXY_HIDE:
+			GP_DEBUG(4, "Got GP_PROXY_HIDE");
+			hidden(backend);
+		break;
+		case GP_PROXY_CURSOR_POS:
+			GP_DEBUG(4, "Got GP_PROXY_CURSOR_POS");
+			gp_ev_queue_set_cursor_pos(backend->event_queue,
+			                           msg->cursor.pos.x,
+			                           msg->cursor.pos.y);
+		break;
+		case GP_PROXY_EXIT:
+			GP_DEBUG(4, "Got GP_PROXY_EXIT");
+			gp_ev_queue_push(backend->event_queue, GP_EV_SYS,
+			                 GP_EV_SYS_QUIT, 0, 0);
+			priv->sys_quit_requested = 1;
+			priv->update_in_progress = 0;
+		break;
+		case GP_PROXY_UPDATE:
+			GP_DEBUG(4, "Got GP_PROXY_UPDATE %i %i %i %i",
+				 msg->rect.rect.x, msg->rect.rect.y, msg->rect.rect.w, msg->rect.rect.h);
+			priv->update_in_progress = 0;
+		break;
+		}
+	}
+
+	return ret;
+}
+
+static enum gp_poll_event_ret proxy_process_fd(gp_fd *self)
+{
+	while (proxy_recv_events(self, 0) > 0);
 
 	return 0;
 }
@@ -235,6 +264,10 @@ static void proxy_update_rect(gp_backend *self, gp_coord x0, gp_coord y0,
 	GP_DEBUG(4, "Sending GP_PROXY_UPDATE");
 
 	gp_proxy_send(priv->fd.fd, GP_PROXY_UPDATE, &rect);
+
+	priv->update_in_progress = 1;
+	while (priv->update_in_progress)
+		proxy_recv_events(&priv->fd, 1);
 }
 
 static void proxy_flip(gp_backend *self)

demos/c_simple/proxy_backend.c
@@ -64,12 +64,12 @@ static void shm_update(gp_proxy_cli *self, gp_coord x, gp_coord y, gp_size w, gp
 		h = screen_h;
 	}
 
-	printf("%i %i %u %u\n", x, y, w, h);
-
 	//TODO: Check SIZE!!!
 	gp_blit_xywh_clipped(&shm->pixmap, x, y, w, h, backend->pixmap, x, y);
 
 	gp_backend_update_rect_xywh(backend, x, y, w, h);
+
+	gp_proxy_cli_rect_updated(self, x, y, w, h);
 }
 
 static void do_exit(void)

include/backends/gp_proxy_cli.h
@@ -116,6 +116,33 @@ static inline void gp_proxy_cli_event(gp_proxy_cli *self, gp_event *ev)
 }
 
 /**
+ * @brief Tells client that requested update was finished.
+ *
+ * This is called by the proxy backend after it finished copying data from the
+ * shared buffer. After client receives this event it can resume drawing to the
+ * area for which update was pending.
+ *
+ * @param self A client (application).
+ * @param x A rect x coordinate.
+ * @param y A rect y coordinate.
+ * @param w A rect width.
+ * @param h A rect height.
+ */
+static inline void gp_proxy_cli_rect_updated(gp_proxy_cli *self,
+                                             uint32_t x, uint32_t y,
+                                             uint32_t w, uint32_t h)
+{
+	struct gp_proxy_rect_ rect = {
+		.x = x,
+		.y = y,
+		.w = w,
+		.h = h,
+	};
+
+	gp_proxy_cli_send(self, GP_PROXY_UPDATE, &rect);
+}
+
+/**
  * @brief A function to fill the proxy client buffer.
  *
  * Has to be called when there are data ready at client fd. The buffer then has

include/backends/gp_proxy_proto.h
@@ -330,9 +330,10 @@ int gp_proxy_next(gp_proxy_buf *buf, gp_proxy_msg **msg);
  *
  * @param fd File descriptor connected to the server/client.
  * @param buf Proxy buffer to store data to.
+ * @param block If set recv blocks until data are received.
  * @return Number of bytes stored into the buffer, -1 on failure.
  */
-int gp_proxy_buf_recv(int fd, gp_proxy_buf *buf);
+int gp_proxy_buf_recv(int fd, gp_proxy_buf *buf, int block);
 
 /**
  * @brief Sends a message to the server/client.

libs/backends/linux/gp_backend_proxy_cli.c
@@ -48,7 +48,7 @@ int gp_proxy_cli_read(struct gp_proxy_cli *self)
 {
 	ssize_t ret;
 
-	ret = gp_proxy_buf_recv(self->fd.fd, &self->buf);
+	ret = gp_proxy_buf_recv(self->fd.fd, &self->buf, 0);
 
 	if (ret > 0) {
 		GP_DEBUG(4, "Client (%p) '%s' read %zu bytes",

libs/backends/linux/gp_backend_proxy_proto.c
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: LGPL-2.0-or-later
 /*
 
-   Copyright (c) 2019-2020 Cyril Hrubis <metan@ucw.cz>
+   Copyright (c) 2019-2025 Cyril Hrubis <metan@ucw.cz>
 
  */
 
@@ -34,13 +34,13 @@ static int validate_msg(const union gp_proxy_msg *msg, unsigned int size)
 	return msg->size;
 }
 
-int gp_proxy_buf_recv(int fd, struct gp_proxy_buf *buf)
+int gp_proxy_buf_recv(int fd, struct gp_proxy_buf *buf, int block)
 {
 	ssize_t ret;
 	char *bufp = buf->buf + buf->pos + buf->size;
 	size_t len = GP_PROXY_BUF_SIZE - buf->pos - buf->size;
 
-	ret = recv(fd, bufp, len, MSG_DONTWAIT);
+	ret = recv(fd, bufp, len, block ? 0 : MSG_DONTWAIT);
 	if (ret > 0)
 		buf->size += ret;
 

commit fb1b98f7ae838032d2a1ef24047b6df429e25fe3

backends: proxy: Clear update_in_progress in hide()
When the proxy WM hides an application in the middle of an update it may
get up stuck with the update_in_progress flag set. We need to clear the
flag on hide() so that that the application can proceed back to the main
loop in this case.

Fixes: 2c7aad7f709c ("backends: proxy: Synchronize backend updates.")
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/linux/gp_backend_proxy.c
@@ -110,6 +110,7 @@ static void hidden(gp_backend *self)
 	//TODO: remap GP_PROXY_NONE?
 
 	priv->visible = 0;
+	priv->update_in_progress = 0;
 
 	self->pixmap = &priv->dummy;
 }