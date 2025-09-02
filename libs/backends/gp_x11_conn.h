gfxprim/gfxprim

commit c36f3e36f7d4fcc23090b365d42f6b76a172791f

backends: x11: Fix cursor hide/show
The XFixes calls to show and hide cursor are broken these calls affect
whole Xserver instead of just the window they take as an argument.

So instead we create an empty cursor and use that to hide cursor.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/gp_x11_conn.h
@@ -36,6 +36,8 @@ struct x11_conn {
 	Cursor cursor_text_edit;
 	Cursor cursor_crosshair;
 	Cursor cursor_hand;
+	Cursor cursor_empty;
+	Cursor cursor_saved;
 
 	/* reference counter, incremented on window creation */
 	unsigned int ref_cnt;
@@ -102,12 +104,31 @@ static void x11_detect_wm_features(void)
 	}
 }
 
+static void create_empty_cursor(void)
+{
+
+	Pixmap empty_bitmap;
+	XColor black = {.red = 0, .blue = 0, .green = 0};
+	static char empty_pixels[] = {0, 0, 0, 0, 0, 0, 0, 0};
+	Window root_win = XDefaultRootWindow(x11_conn.dpy);
+
+	empty_bitmap = XCreateBitmapFromData(x11_conn.dpy, root_win, empty_pixels, 8, 8);
+	x11_conn.cursor_empty = XCreatePixmapCursor(x11_conn.dpy,
+	                                            empty_bitmap, empty_bitmap,
+	                                            &black, &black, 0, 0);
+	XDefineCursor(x11_conn.dpy, root_win, x11_conn.cursor_empty);
+}
+
 static void x11_get_cursors(void)
 {
 	x11_conn.cursor_arrow = XCreateFontCursor(x11_conn.dpy, XC_left_ptr);
 	x11_conn.cursor_text_edit = XCreateFontCursor(x11_conn.dpy, XC_xterm);
 	x11_conn.cursor_crosshair = XCreateFontCursor(x11_conn.dpy, XC_tcross);
 	x11_conn.cursor_hand = XCreateFontCursor(x11_conn.dpy, XC_hand2);
+
+	x11_conn.cursor_saved = x11_conn.cursor_arrow;
+
+	create_empty_cursor();
 }
 
 static void x11_input_init(void);

libs/backends/gp_x11.c
@@ -579,21 +579,27 @@ static int x11_set_cursor(gp_backend *self, enum gp_backend_cursor_req cursor)
 
 	switch (cursor) {
 	case GP_BACKEND_CURSOR_HIDE:
-		XFixesHideCursor(win->dpy, win->win);
+	//	XFixesHideCursor(win->dpy, win->win);
+		XDefineCursor(win->dpy, win->win, x11_conn.cursor_empty);
 	break;
 	case GP_BACKEND_CURSOR_SHOW:
-		XFixesShowCursor(win->dpy, win->win);
+	//	XFixesShowCursor(win->dpy, win->win);
+		XDefineCursor(win->dpy, win->win, x11_conn.cursor_saved);
 	break;
 	case GP_BACKEND_CURSOR_ARROW:
+		x11_conn.cursor_saved = x11_conn.cursor_arrow;
 		XDefineCursor(win->dpy, win->win, x11_conn.cursor_arrow);
 	break;
 	case GP_BACKEND_CURSOR_TEXT_EDIT:
+		x11_conn.cursor_saved = x11_conn.cursor_text_edit;
 		XDefineCursor(win->dpy, win->win, x11_conn.cursor_text_edit);
 	break;
 	case GP_BACKEND_CURSOR_CROSSHAIR:
+		x11_conn.cursor_saved = x11_conn.cursor_crosshair;
 		XDefineCursor(win->dpy, win->win, x11_conn.cursor_crosshair);
 	break;
 	case GP_BACKEND_CURSOR_HAND:
+		x11_conn.cursor_saved = x11_conn.cursor_hand;
 		XDefineCursor(win->dpy, win->win, x11_conn.cursor_hand);
 	break;
 	default:

commit a517cb9eaf3ddf41abd721efa44b9e79d7244bef

backends: x11: Do not set empty cursor for root win
The broken commit accidentally set empty cursor for the root window.

Fixes: c36f3e36f7d4 ('backends: x11: Fix cursor hide/show')
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/gp_x11_conn.h
@@ -116,7 +116,6 @@ static void create_empty_cursor(void)
 	x11_conn.cursor_empty = XCreatePixmapCursor(x11_conn.dpy,
 	                                            empty_bitmap, empty_bitmap,
 	                                            &black, &black, 0, 0);
-	XDefineCursor(x11_conn.dpy, root_win, x11_conn.cursor_empty);
 }
 
 static void x11_get_cursors(void)