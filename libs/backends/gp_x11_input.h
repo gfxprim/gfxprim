gfxprim/gfxprim

commit 63e2bc410136d69db2617fdc7117abdb9809ddcb

libs/input: Make the UTF event part of a key event
The keypresses and unicode characters should be delivered in a single
event. So now the key event has two components now. One is a key
scancode and second is a unicode character.

Syntetic unicode characters have the key set to 0 and keypresses that
does not generate unicode characters have utf set to 0.

If a key press generates more than one unicode characters the last
unicode character has the key set to the scancode key.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/gp_x11_input.h
@@ -194,7 +194,7 @@ static void x11_input_event_put(gp_ev_queue *event_queue,
 	int key = 0, press = 0;
 	KeySym keysym;
 	Status status;
-	uint32_t unicode = 0;
+	uint32_t utf = 0;
 	char str[32];
 
 	switch (ev->type) {
@@ -255,10 +255,21 @@ static void x11_input_event_put(gp_ev_queue *event_queue,
 			Xutf8LookupString(win->xic, &ev->xkey, str, sizeof(str)-1, &keysym, &status);
 
 			if (status == XLookupChars || status == XLookupBoth) {
+				key = get_key(ev->xkey.keycode);
 				const char *s = str;
-				unicode = gp_utf8_next(&s);
+				size_t len = gp_utf8_strlen(str);
 
-				gp_ev_queue_push_utf(event_queue, unicode, 0);
+				/*
+				 * Send all characters without a key scancode, the key scancode
+				 * is send to the last one.
+				 */
+				while ((utf = gp_utf8_next(&s))) {
+					gp_ev_queue_push_key(event_queue,
+					                     --len ? 0 : key,
+					                     press, utf, 0);
+				}
+
+				return;
 			}
 		}
 	/* fallthrough */
@@ -268,7 +279,7 @@ static void x11_input_event_put(gp_ev_queue *event_queue,
 		if (key == 0)
 			return;
 
-		gp_ev_queue_push_key(event_queue, key, press, 0);
+		gp_ev_queue_push_key(event_queue, key, press, 0, 0);
 	break;
 	/* events from WM */
 	case ClientMessage:

demos/c_simple/input_example.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2009-2010 Jiri "BlueBear" Dluhos
  *                         <jiri.bluebear.dluhos@gmail.com>
  *
- * Copyright (C) 2009-2013 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2025 Cyril Hrubis <metan@ucw.cz>
  */
 
 #include <stdio.h>
@@ -19,22 +19,17 @@ static gp_pixel red, green, white, black;
 static void draw_event(gp_event *ev)
 {
 	gp_pixmap *win = backend->pixmap;
-	static gp_size ksize = 0, usize = 0, bsize = 0;
+	static gp_size ksize = 0, bsize = 0;
 	int align = GP_ALIGN_RIGHT|GP_VALIGN_BOTTOM;
 
 	switch (ev->type) {
 	case GP_EV_KEY:
 		gp_text_clear(win, NULL, 20, 20, align, black, ksize);
 		ksize = gp_print(win, NULL, 20, 20, align,
-		                 white, black, "Key=%s %u",
+		                 white, black, "Key=%s %u UTF=0x%04x",
 				 gp_ev_key_name(ev->key.key),
-				 (unsigned int)ev->code);
-	break;
-	case GP_EV_UTF:
-		gp_text_clear(win, NULL, 20, 40, align, black, usize);
-		usize = gp_print(win, NULL, 20, 40, align,
-		                 white, black, "UTF=%04x",
-				 ev->utf.ch);
+				 (unsigned int)ev->code,
+				 ev->key.utf);
 	break;
 	case GP_EV_SYS:
 		switch (ev->code) {
@@ -67,9 +62,6 @@ static void event_loop(void)
 			gp_ev_dump(ev);
 
 			switch (ev->type) {
-			case GP_EV_UTF:
-				draw_event(ev);
-			break;
 			case GP_EV_KEY:
 				draw_event(ev);
 

include/input/gp_ev_queue.h
@@ -286,22 +286,14 @@ void gp_ev_queue_push_abs(gp_ev_queue *self,
  *
  * @param self An input queue.
  * @param key Physical key pressed on keyboard.
+ * @param utf An unicode character generated by the keypress. Pass 0 if none
+ *            was generated.
  * @param code Action enum #gp_ev_key_code press/release/repeat.
  * @param time A timestamp, if NULL current time is used.
  */
 void gp_ev_queue_push_key(gp_ev_queue *self,
                           uint32_t key, uint8_t code,
-                          uint64_t time);
-
-/**
- * @brief Pushes an unicode character typed on keyboard into the queue.
- *
- * @param self An input queue.
- * @param utf_ch An unicode character.
- * @param time A timestamp, if NULL current time is used.
- */
-void gp_ev_queue_push_utf(gp_ev_queue *self, uint32_t utf_ch,
-                          uint64_t time);
+                          uint32_t utf, uint64_t time);
 
 /**
  * @brief Pushes a window resize event to the queue.

include/input/gp_event.h
@@ -33,8 +33,6 @@ enum gp_ev_type {
 	GP_EV_SYS = 4,
 	/** @brief A timer expired. */
 	GP_EV_TMR = 5,
-	/** @brief An unicode character typed on a keyboard. */
-	GP_EV_UTF = 6,
 	/** @brief Last used event type. */
 	GP_EV_MAX = 6,
 };
@@ -156,8 +154,24 @@ struct gp_ev_pos_abs {
 	uint32_t pressure, pressure_max;
 };
 
+/**
+ * @brief A key press/release/repeat event.
+ */
 struct gp_ev_key {
+	/**
+	 * @brief One of enum gp_event_key_value.
+	 *
+	 * This is the raw key scancode. It can be set to zero if this is a
+	 * synthetic unicode character.
+	 */
 	uint32_t key;
+	/**
+	 * @brief Unicode character (finalized) by this keystroke.
+	 *
+	 * If non-zero the key press was translated into a character by current
+	 * keyboard mapping.
+	 */
+	uint32_t utf;
 };
 
 /** @brief An system event value. */
@@ -216,8 +230,6 @@ struct gp_event {
 		struct gp_ev_pos_abs abs;
 		/** @brief A system event. */
 		struct gp_ev_sys sys;
-		/** @brief An unicode input event. */
-		struct gp_ev_utf utf;
 		/** @brief A timer expired event. */
 		gp_timer *tmr;
 	};
@@ -233,11 +245,14 @@ struct gp_event {
  * @brief Checks if a character is not printable.
  *
  * @param ev A key input event.
- * @return True if ch is a control character (non-printable).
+ * @return True if utf character is a control character (non-printable).
  */
 static inline int gp_ev_utf_is_ctrl(gp_event *ev)
 {
-	return ev->utf.ch < 0x20 && ev->utf.ch != 0x7f;
+	if (!ev->key.utf)
+		return 0;
+
+	return ev->key.utf < 0x20 && ev->key.utf != 0x7f;
 }
 
 /**

libs/backends/gp_aalib.c
@@ -163,8 +163,8 @@ static void parse_event(gp_backend *self, int ev)
 	}
 
 	/* emulate keyup events */
-	gp_ev_queue_push_key(self->event_queue, key, 1, 0);
-	gp_ev_queue_push_key(self->event_queue, key, 0, 0);
+	gp_ev_queue_push_key(self->event_queue, key, 1, 0, 0);
+	gp_ev_queue_push_key(self->event_queue, key, 0, 0, 0);
 }
 
 static void aalib_poll(gp_backend *self)

libs/backends/gp_backend_wayland.c
@@ -245,7 +245,7 @@ pointer_handle_button(void *data, struct wl_pointer UN(*pointer),
 	struct client_state *state = data;
 	gp_backend *backend = state->backend;
 
-	gp_ev_queue_push_key(backend->event_queue, button, button_state, 0);
+	gp_ev_queue_push_key(backend->event_queue, button, button_state, 0, 0);
 }
 
 static void
@@ -331,16 +331,25 @@ keyboard_handle_key(void *data, struct wl_keyboard UN(*keyboard),
 	int ret;
 	char buf[128];
 
-	gp_ev_queue_push_key(backend->event_queue, key, key_state, 0);
-
 	ret = xkb_state_key_get_utf8(state->keymap_state, key+8, buf, sizeof(buf));
 	if (ret) {
-		uint32_t unicode;
+		uint32_t utf;
 		const char *str = buf;
+		size_t len = gp_utf8_strlen(str);
 
-		while ((unicode = gp_utf8_next(&str)))
-			gp_ev_queue_push_utf(backend->event_queue, unicode, 0);
+		/*
+		 * Send all characters without a key scancode, the key scancode
+		 * is send to the last one.
+		 */
+		while ((utf = gp_utf8_next(&str))) {
+			gp_ev_queue_push_key(backend->event_queue,
+			                     --len ? 0 : key, key_state, utf, 0);
+		}
+
+		return;
 	}
+
+	gp_ev_queue_push_key(backend->event_queue, key, key_state, 0, 0);
 }
 
 static void

libs/backends/gp_input_driver_sdl.c
@@ -194,7 +194,13 @@ void gp_input_driver_sdl_event_put(gp_backend *backend, gp_ev_queue *event_queue
 		}
 #endif
 
-		gp_ev_queue_push_key(event_queue, key, ev->key.state, 0);
+		gp_ev_queue_push_key(event_queue, key, ev->key.state,
+#if LIBSDL_VERSION == 1
+				ev->key.keysym.unicode,
+#else
+				0, //TODO Unicode handling in libsdl2.0?
+#endif
+				0);
 
 #if LIBSDL_VERSION == 1
 		//KEYMAP CTRL

libs/backends/gp_xcb_input.h
@@ -241,7 +241,8 @@ static void xcb_input_event_put(gp_ev_queue *event_queue, struct win *win,
 		if (key == 0)
 			return;
 
-		gp_ev_queue_push_key(event_queue, key, press, 0);
+		//TODO: unicode!
+		gp_ev_queue_push_key(event_queue, key, press, 0, 0);
 	} break;
 	/* events from WM */
 	case XCB_CLIENT_MESSAGE: {

commit eb90c55a54bb01018f484c7a24d6627d0f010fab

backends: x11: Fix utf key press handling
In certain cases the XUtf8LookupString() returns an empty string in
which case we have to report the keypress regardless.

Fixes: 63e2bc410136 ('libs/input: Make the UTF event part of a key event')
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/backends/gp_x11_input.h
@@ -251,10 +251,9 @@ static void x11_input_event_put(gp_ev_queue *event_queue,
 		press = 1;
 
 		if (!XFilterEvent(ev, None)) {
-
 			Xutf8LookupString(win->xic, &ev->xkey, str, sizeof(str)-1, &keysym, &status);
 
-			if (status == XLookupChars || status == XLookupBoth) {
+			if ((status == XLookupChars || status == XLookupBoth) && str[0]) {
 				key = get_key(ev->xkey.keycode);
 				const char *s = str;
 				size_t len = gp_utf8_strlen(str);
@@ -268,7 +267,6 @@ static void x11_input_event_put(gp_ev_queue *event_queue,
 					                     --len ? 0 : key,
 					                     press, utf, 0);
 				}
-
 				return;
 			}
 		}