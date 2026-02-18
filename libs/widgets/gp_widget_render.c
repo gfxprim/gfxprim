gfxprim/gfxprim

commit 6023eca456c340165186140ee02f8e23b2731e74

backends: redesing resize_ack()
Make the rendering event based. Previously after backend has been
initialzied the backend->pixmap was used to determine pixel type.

Now there is a GP_EV_SYS_RENDER_PIXEL_TYPE vent that is send to
applicaton once pixel type has been determined so that application
can prepare the colors.

Then GP_EV_SYS_RENDER_RESIZE event is send and the application can
prepare the rendering for resize.

And the rendering is finally started by GP_EV_SYS_RENDER_START from that
point on the backend->pixmap pointer is valid and can be used for
rendering.

When the backend pixmap needs to be resized or when the application is
not shown on the screen the GP_EV_SYS_RENDER_STOP event is send, which
is acked by the application with gp_backend_render_stopped(). The
backend->pixmap pointer is set to NULL during the time rendering is
stopped.

Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_render.c
@@ -615,8 +615,11 @@ int gp_widgets_event(gp_event *ev, gp_widget *layout)
 	break;
 	case GP_EV_SYS:
 		switch (ev->code) {
-		case GP_EV_SYS_RESIZE:
-			gp_backend_resize_ack(backend);
+		case GP_EV_SYS_RENDER_STOP:
+			gp_backend_render_stopped(backend);
+			ctx.buf = NULL;
+		break;
+		case GP_EV_SYS_RENDER_START:
 			ctx.buf = backend->pixmap;
 			gp_fill(backend->pixmap, ctx.fill_color);
 			gp_widget_render(layout, &ctx, GP_WIDGET_RESIZE);
@@ -662,7 +665,7 @@ int gp_widgets_process_events(gp_widget *layout)
 	gp_event *ev;
 
 	while ((ev = gp_backend_ev_poll(backend))) {
-		//gp_event_dump(&ev);
+		gp_ev_dump(ev);
 		//fflush(stdout);
 		if (gp_widgets_event(ev, layout))
 			gp_widgets_exit(0);

demos/bogoman/bogoman.c
@@ -82,8 +82,11 @@ static void event_loop(struct bogoman_render *render, gp_backend *backend)
 		break;
 		case GP_EV_SYS:
 			switch (ev->code) {
-			case GP_EV_SYS_RESIZE:
-				gp_backend_resize_ack(backend);
+			case GP_EV_SYS_RENDER_STOP:
+				gp_backend_render_stopped(backend);
+				render->pixmap = NULL;
+			break;
+			case GP_EV_SYS_RENDER_START:
 				render->pixmap = backend->pixmap;
 				bogoman_render(render, BOGOMAN_RENDER_ALL);
 			break;

demos/bogoman/bogoman_render.c
@@ -250,6 +250,9 @@ void bogoman_render(struct bogoman_render *render, int flags)
 {
 	unsigned int x, y;
 
+	if (!render->pixmap)
+		return;
+
 	//TODO: Hack
 	init_colors(render->pixmap, &colors);
 

demos/c_simple/.gitignore
@@ -30,7 +30,6 @@ SDL2_glue
 /v4l2_grab
 /v4l2_show
 /version
-/virtual_backend_example
 /weighted_median
 /x11_windows
 /zip_container

demos/c_simple/Makefile
@@ -22,10 +22,9 @@ INCLUDE=
 LDLIBS+=-lrt -lgfxprim
 
 APPS=backend_example loaders_example loaders filters_symmetry gfx_koch\
-     virtual_backend_example meta_data showimage\
-     v4l2_show v4l2_grab convolution weighted_median shapetest koch \
-     input_example fileview linetest randomshapetest fonttest\
-     loaders_register blittest textaligntest x11_windows\
+     meta_data showimage v4l2_show v4l2_grab convolution weighted_median\
+     shapetest koch input_example fileview linetest randomshapetest\
+     fonttest loaders_register blittest textaligntest x11_windows\
      debug_handler gaussian_noise version pretty_print timers\
      zip_container backend_timers_example memory_io data_storage\
      vector matrix proxy_backend bbox backend_tasks_example json_writer\
@@ -50,7 +49,6 @@ backend_example: LDLIBS+=-lgfxprim-backends
 backend_cursor: LDLIBS+=-lgfxprim-backends
 backend_sw_cursor: LDLIBS+=-lgfxprim-backends
 backend_timers_example: LDLIBS+=-lgfxprim-backends
-virtual_backend_example: LDLIBS+=-lgfxprim-backends -lm
 loaders_example: LDLIBS+=-lgfxprim-loaders
 loaders: LDLIBS+=-lgfxprim-loaders
 filters_symmetry: LDLIBS+=-lgfxprim-loaders

demos/c_simple/backend_cursor.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.1-or-later
 /*
- * Copyright (C) 2009-2024 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2026 Cyril Hrubis <metan@ucw.cz>
  */
 
  /*
@@ -80,8 +80,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	redraw(backend);
-
 	/* Handle events */
 	for (;;) {
 		gp_event *ev = gp_backend_ev_wait(backend);
@@ -112,8 +110,10 @@ int main(int argc, char *argv[])
 		break;
 		case GP_EV_SYS:
 			switch (ev->code) {
-			case GP_EV_SYS_RESIZE:
-				gp_backend_resize_ack(backend);
+			case GP_EV_SYS_RENDER_STOP:
+				gp_backend_render_stopped(backend);
+			break;
+			case GP_EV_SYS_RENDER_START:
 				redraw(backend);
 			break;
 			case GP_EV_SYS_QUIT:

demos/c_simple/backend_example.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.1-or-later
 /*
- * Copyright (C) 2009-2013 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2026 Cyril Hrubis <metan@ucw.cz>
  */
 
  /*
@@ -17,6 +17,9 @@ static void redraw(gp_backend *self)
 	gp_pixmap *pixmap = self->pixmap;
 	gp_pixel white_pixel, black_pixel;
 
+	if (!pixmap)
+		return;
+
 	black_pixel = gp_rgb_to_pixmap_pixel(0x00, 0x00, 0x00, pixmap);
 	white_pixel = gp_rgb_to_pixmap_pixel(0xff, 0xff, 0xff, pixmap);
 
@@ -24,7 +27,6 @@ static void redraw(gp_backend *self)
 	gp_line(pixmap, 0, 0, pixmap->w - 1, pixmap->h - 1, white_pixel);
 	gp_line(pixmap, 0, pixmap->h - 1, pixmap->w - 1, 0, white_pixel);
 
-	/* Update the backend screen */
 	gp_backend_flip(self);
 }
 
@@ -55,8 +57,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	redraw(backend);
-
 	/* Handle events */
 	for (;;) {
 		gp_event *ev = gp_backend_ev_wait(backend);
@@ -75,8 +75,10 @@ int main(int argc, char *argv[])
 		break;
 		case GP_EV_SYS:
 			switch (ev->code) {
-			case GP_EV_SYS_RESIZE:
-				gp_backend_resize_ack(backend);
+			case GP_EV_SYS_RENDER_STOP:
+				gp_backend_render_stopped(backend);
+			break;
+			case GP_EV_SYS_RENDER_START:
 				redraw(backend);
 			break;
 			case GP_EV_SYS_QUIT:

demos/c_simple/backend_tasks_example.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.1-or-later
 /*
- * Copyright (C) 2009-2021 Cyril Hrubis <metan@ucw.cz>
+ * Copyright (C) 2009-2026 Cyril Hrubis <metan@ucw.cz>
  */
 
  /*
@@ -31,6 +31,9 @@ static int redraw_task_callback(gp_task *self)
 {
 	gp_backend *backend = self->priv;
 	gp_pixel red_pixel;
+
+	if (!backend->pixmap)
+		return 0;
 
 	red_pixel = gp_rgb_to_pixmap_pixel(0xff, 0x00, 0x00, backend->pixmap);
 
@@ -71,8 +74,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	redraw(backend);
-
 	gp_backend_task_queue_set(backend, &task_queue);
 
 	gp_task redraw_task = {
@@ -101,8 +102,10 @@ int main(int argc, char *argv[])
 		break;
 		case GP_EV_SYS:
 			switch (ev->code) {
-			case GP_EV_SYS_RESIZE:
-				gp_backend_resize_ack(backend);
+			case GP_EV_SYS_RENDER_STOP:
+				gp_backend_render_stopped(backend);
+			break;
+			case GP_EV_SYS_RENDER_START:
 				redraw(backend);
 			break;
 			case GP_EV_SYS_QUIT:

commit 3d7e97b52797d7ddeee22e08f619560cefe1fb44

widgets: render: Fix crashes
Do not attempt to draw to the backend pixmap when rendering is stopped.

Fixes: 6023eca456c3 ("backends: redesing resize_ack()")
Signed-off-by: Cyril Hrubis <metan@ucw.cz>

libs/widgets/gp_widget_render.c
@@ -383,6 +383,9 @@ void gp_widgets_redraw(struct gp_widget *layout)
 	if (!layout->redraw && !layout->redraw_child)
 		return;
 
+	if (!backend->pixmap)
+		return;
+
 	if (back_from_dialog) {
 		back_from_dialog = 0;
 		gp_widget_calc_size(layout, &ctx, 0, 0, 1);
@@ -665,7 +668,7 @@ int gp_widgets_process_events(gp_widget *layout)
 	gp_event *ev;
 
 	while ((ev = gp_backend_ev_poll(backend))) {
-		gp_ev_dump(ev);
+		//gp_ev_dump(ev);
 		//fflush(stdout);
 		if (gp_widgets_event(ev, layout))
 			gp_widgets_exit(0);